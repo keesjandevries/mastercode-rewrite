95b898ed5d036bb58e5c5f1ae9a3b7ad76004cd8
Sam Rogerson <samr@cern.ch>
2012-05-25 15:55:18 +0200

    neaten up, makefile next

--- softsusy_pars/compile_ss.sh
+++ softsusy_pars/compile_ss.sh
@@ -10,5 +10,8 @@ if [[ ! -h softsusy-3.3.1/.libs/libsoft.so ]]; then
     cd ${MAINDIR}
 fi
 
-g++ -c -fPIC -o softsusy_interface.o softsusy_interface.cc -I${MAINDIR}/softsusy-3.3.1/ -L${MAINDIR}/softsusy-3.3.1/.libs -lsoft
-g++ -shared -Wl,-soname,libmcsoftsusy.so -Wl,-rpath,${MAINDIR}/softsusy-3.3.1/.libs -o libmcsoftsusy.so softsusy_interface.o -L${MAINDIR}/softsusy-3.3.1/.libs -lsoft
+g++ -c -fPIC -o softsusy_interface.o softsusy_interface.cc \
+    -I${MAINDIR}/softsusy-3.3.1/ -L${MAINDIR}/softsusy-3.3.1/.libs -lsoft
+g++ -shared -Wl,-soname,libmcsoftsusy.so \
+    -Wl,-rpath,${MAINDIR}/softsusy-3.3.1/.libs -o libmcsoftsusy.so \
+   softsusy_interface.o -L${MAINDIR}/softsusy-3.3.1/.libs -lsoft
