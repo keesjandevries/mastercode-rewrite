66343f9bffa20edec0c65d0f011b005a2ea01f9f
Sam Rogerson <samr@cern.ch>
2012-05-25 13:00:31 +0200

    fix type

--- softsusy_work/softsusy_module.py
+++ softsusy_work/softsusy_module.py
@@ -22,20 +22,20 @@ class ss_QedQcd(object) :
     def __init__(self) :
         self.obj = SPlib.QedQcd_new()
     def setPoleMt(self,  mt) :
-        SPlib.QedQcd_setPoleMt( self.obj, mt )
+        SPlib.QedQcd_setPoleMt( self.obj, c_double(mt) )
     def setPoleMb(self, mb) :
-        SPlib.QedQcd_setPoleMb( self.obj, mb )
+        SPlib.QedQcd_setPoleMb( self.obj, c_double(mb) )
     def setPoleMtau(self, mtau) :
-        SPlib.QedQcd_setPoleMtau( self.obj, mtau )
+        SPlib.QedQcd_setPoleMtau( self.obj, c_double(mtau) )
     def setMbMb(self, mb) :
-        SPlib.QedQcd_setMbMb( self.obj, mb )
+        SPlib.QedQcd_setMbMb( self.obj, c_double(mb) )
     # these need enums equivalents...
     def setMass( self, mno, m) :
         mno = c_int(mno) #enum type
-        SPlib.QedQcd_setMass( self.obj, mno, m )
+        SPlib.QedQcd_setMass( self.obj, mno, c_double(m) )
     def setAlpha( self, ai, ap) :
         ai = c_int(ai) #enum type
-        SPlib.QedQcd_setAlpha( self.obj, ai, ap)
+        SPlib.QedQcd_setAlpha( self.obj, ai, c_double(ap))
     def set( self, dv ) :
         SPlib.QedQcd_set( dv )
 
@@ -49,3 +49,4 @@ print test[1]
 test_mm = ss_MssmSoftsusy()
 test_qq = ss_QedQcd()
 test_qq.setPoleMt(173.2)
+test_qq.setMass(3,173.2)
