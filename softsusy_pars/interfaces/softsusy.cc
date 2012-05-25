46ca893b637dd58b4d63a2727a6e7855df0dc1df
Sam Rogerson <samr@cern.ch>
2012-05-25 17:27:33 +0200

    move around

---
+++ softsusy_pars/interfaces/softsusy.cc
@@ -0,0 +1,122 @@
+#include "linalg.h"
+#include "softsusy.h"
+#include "lowe.h"
+#include <iostream>
+
+/*--------------------*/
+/* BoundaryConditions */
+/*--------------------*/
+void (*boundaryCondition( int cond ))( MssmSoftsusy &, const DoubleVector &) {
+    void (*retval)(MssmSoftsusy &, const DoubleVector &);
+    switch (cond) {
+        case 0 :
+            retval = &sugraBcs;
+            break;
+        case 1 :
+            retval = &extendedSugraBcs;
+            break;
+        case 2 :
+            retval = &generalBcs;
+            break;
+        case 3 :
+            retval = &generalBcs2;
+            break;
+        case 4 :
+            retval = &amsbBcs;
+            break;
+        case 5 :
+            retval = &gmsbBcs;
+            break;
+        case 6 :
+            retval = &splitGmsb;
+            break;
+        case 7 :
+            retval = &lvsBcs;
+            break;
+        case 8 :
+            retval = &nonUniGauginos;
+            break;
+        case 9 :
+            retval = &userDefinedBcs;
+            break;
+        case 10 :
+            retval = &nonUniGauginos;
+            break;
+        default : 
+            std::cout << "Defaulting to sugra boundary conditions" << 
+                std::endl;
+            retval = &sugraBcs;
+    }
+    return retval;
+}
+
+extern "C" 
+{
+    /*--------------*/
+    /* DoubleVector */
+    /*--------------*/
+    DoubleVector* DoubleVector_new( int sz ) {
+        return new DoubleVector(sz);
+    }
+    double DoubleVector_display( DoubleVector* dv, int pos )  { 
+        return dv->display(pos+1); 
+    }
+    void DoubleVector_set( DoubleVector* dv, int pos, double val )  { 
+        (*dv)(pos+1) = val;
+    }
+
+    /*--------*/
+    /* QedQcd */
+    /*--------*/
+    QedQcd* QedQcd_new() {
+        return new QedQcd();
+    }
+    void QedQcd_setPoleMt(QedQcd *qq, double mt) { 
+        qq->setPoleMt(mt); 
+    }
+    void QedQcd_setPoleMb(QedQcd *qq, double mb) {
+        qq->setPoleMb(mb); 
+    }
+    void QedQcd_setPoleMtau(QedQcd *qq, double mtau) {
+        qq->setPoleMtau(mtau); 
+    }
+    void QedQcd_setMbMb(QedQcd *qq, double mb)   {
+        qq->setMbMb(mb);   
+    }
+    void QedQcd_setMass(QedQcd *qq, mass mno, double m) {
+        qq->setMass(mno,m);
+    } 
+    void QedQcd_setAlpha(QedQcd *qq, leGauge ai, double ap) {
+        //  typedef enum {mUp=1, mCharm, mTop, mDown, mStrange, mBottom, mElectron,
+        //            mMuon, mTau} mass;
+        //  typedef enum {ALPHA=1, ALPHAS} leGauge;
+        qq->setAlpha(ai, ap);
+    } 
+    void QedQcd_set(QedQcd *qq, DoubleVector *dv) {
+        qq->set(*dv);
+    }
+
+    /*--------------*/
+    /* MssmSoftsusy */
+    /*--------------*/
+    MssmSoftsusy* MssmSoftsusy_new() {
+        return new MssmSoftsusy();
+    }
+
+    void MssmSoftsusy_lowOrg( MssmSoftsusy* mss, int bCond, double mxGuess,
+                              DoubleVector *pars, int sgnMu, double tanb,
+                              QedQcd *oneset, bool gaugeUnification,
+                              bool ewsbBCscale = false) {
+        void (*bC)( MssmSoftsusy &, const DoubleVector &) = boundaryCondition( bCond );
+        mss->lowOrg(bC, mxGuess, *pars, sgnMu, tanb, *oneset, 
+                    gaugeUnification, ewsbBCscale);
+    }
+
+    void MssmSoftsusy_lesHouchesAccordOutput( MssmSoftsusy* mss, const char model[], 
+					  DoubleVector *pars, int sgnMu, double tanb, double qMax, 
+					  int numPoints, double mgut, bool altEwsb) {
+        //ostream & out
+        mss->lesHouchesAccordOutput( std::cout, model, *pars, sgnMu, tanb, qMax,
+                                     numPoints, mgut, altEwsb );
+    }
+} 
---
+++ softsusy_pars/interfaces/softsusy.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+
+from ctypes import cdll, c_int, c_double, c_char_p
+
+SPlib = cdll.LoadLibrary('./libs/libmcsoftsusy.so')
+# set our return types
+SPlib.DoubleVector_display.restype = c_double
+boundaryConditions = [ "sugraBcs", "extendedSugaBcs", "generalBcs",
+                       "generalBcs2", "amsbBcs", "gmsbBcs", "splitGmsb",
+                       "lvsBcs", "nonUniGauginos", "userDefinedBcs",
+                       "nonUniGauginos", ]
+
+class DoubleVector(object) :
+    def __init__(self, size = 0) :
+        self.obj = SPlib.DoubleVector_new( size )
+    def __getitem__( self, i ) :
+        return SPlib.DoubleVector_display( self.obj, i )
+    def __setitem__( self, index, value ) :
+        SPlib.DoubleVector_set( self.obj, index, c_double(value) )
+ 
+class MssmSoftsusy(object) :
+    def __init__(self) :
+        self.obj = SPlib.MssmSoftsusy_new()
+    def lowOrg(self, bCond, mxGuess, dv_pars, sgnMu, tanb, qq_oneset,
+               gaugeUnification, ewsbBCscale = False ) :
+        mxGuess = c_double(mxGuess)
+        tanb = c_double(tanb)
+        bC = boundaryConditions.index( bCond )
+        SPlib.MssmSoftsusy_lowOrg( self.obj, bC, mxGuess, dv_pars.obj, sgnMu,
+                                   tanb, qq_oneset.obj, gaugeUnification,
+                                   ewsbBCscale )
+    def lesHouchesAccordOutput( self, model, dv_pars, sgnMu, tanb, qMax, 
+                                numPoints, mgut, altEwsb ) :
+        tanb = c_double(tanb)
+        qMax = c_double(qMax)
+        mgut = c_double(mgut)
+        model = c_char_p( model )
+        SPlib.MssmSoftsusy_lesHouchesAccordOutput( self.obj, model, dv_pars.obj,
+                                                   sgnMu, tanb, qMax, numPoints,
+                                                   mgut, altEwsb )
+
+class QedQcd(object) :
+    def __init__(self) :
+        self.obj = SPlib.QedQcd_new()
+    def setPoleMt(self,  mt) :
+        SPlib.QedQcd_setPoleMt( self.obj, c_double(mt) )
+    def setPoleMb(self, mb) :
+        SPlib.QedQcd_setPoleMb( self.obj, c_double(mb) )
+    def setPoleMtau(self, mtau) :
+        SPlib.QedQcd_setPoleMtau( self.obj, c_double(mtau) )
+    def setMbMb(self, mb) :
+        SPlib.QedQcd_setMbMb( self.obj, c_double(mb) )
+    # these need enums equivalents...
+    def setMass( self, mno, m) :
+        mno = c_int(mno) #enum type
+        SPlib.QedQcd_setMass( self.obj, mno, c_double(m) )
+    def setAlpha( self, ai, ap) :
+        ai = c_int(ai) #enum type
+        SPlib.QedQcd_setAlpha( self.obj, ai, c_double(ap))
+    def set( self, dv ) :
+        SPlib.QedQcd_set( dv )
