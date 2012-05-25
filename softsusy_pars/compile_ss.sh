8471016ea07660603fe62bebde8b180ecd85c014
Sam Rogerson <samr@cern.ch>
2012-05-25 15:52:12 +0200

    make ss if not there

--- softsusy_pars/compile_ss.sh
+++ softsusy_pars/compile_ss.sh
@@ -1,3 +1,14 @@
 MAINDIR=`pwd`  
+
+if [[ ! -h softsusy-3.3.1/.libs/libsoft.so ]]; then
+    if [[ ! -d softsusy-3.3.1 ]]; then
+        tar zxf softsusy-3.3.1.tar.gz
+    fi
+    cd softsusy-3.3.1/
+    ./configure
+    make
+    cd ${MAINDIR}
+fi
+
 g++ -c -fPIC -o softsusy_interface.o softsusy_interface.cc -I${MAINDIR}/softsusy-3.3.1/ -L${MAINDIR}/softsusy-3.3.1/.libs -lsoft
 g++ -shared -Wl,-soname,libmcsoftsusy.so -Wl,-rpath,${MAINDIR}/softsusy-3.3.1/.libs -o libmcsoftsusy.so softsusy_interface.o -L${MAINDIR}/softsusy-3.3.1/.libs -lsoft
