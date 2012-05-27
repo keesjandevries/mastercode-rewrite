d420d0e180c749ef6d1e52746880b3d4bf6576c8
Sam Rogerson <samr@cern.ch>
2012-05-27 16:37:25 +0200

    test file timing

--- softsusy_pars/testPoint.py
+++ softsusy_pars/testPoint.py
@@ -2,26 +2,33 @@ from interfaces.softsusy import (DoubleVector, MssmSoftsusy, QedQcd)
 from interfaces.slha import SLHAfile
 
 #! /usr/bin/env python
-# DoubleVector
-inputs = DoubleVector(3)
-inputs[0] = 100.
-inputs[1] = 200.
-inputs[2] = 0.
-tanb = 10.
-sgnMu = 1
-mgut = 2e16
+def testPoint() :
+    # DoubleVector
+    inputs = DoubleVector(3)
+    inputs[0] = 100.
+    inputs[1] = 200.
+    inputs[2] = 0.
+    tanb = 10.
+    sgnMu = 1
+    mgut = 2e16
 
-# setup MssmSoftusy object
-r = MssmSoftsusy()
+    # setup MssmSoftusy object
+    r = MssmSoftsusy()
 
-# QedQcd object
-oneset = QedQcd()
-oneset.setPoleMt(173.2)
-oneset.setMass(3,173.2)
-r.lowOrg( "sugraBcs", mgut, inputs, sgnMu, tanb, oneset, False )
-#r.lesHouchesAccordOutput( "sugra", inputs, sgnMu, tanb, 91.1875,  1, mgut, False )
+    # QedQcd object
+    oneset = QedQcd()
+    oneset.setPoleMt(173.2)
+    oneset.setMass(3,173.2)
+    r.lowOrg( "sugraBcs", mgut, inputs, sgnMu, tanb, oneset, False )
+    #r.lesHouchesAccordOutput( "sugra", inputs, sgnMu, tanb, 91.1875,  1, mgut, False )
 
-slhafile = SLHAfile()
-slhafile.ReadFile("")
-r.lesHouchesAccordOutputStream( "sugra", inputs, sgnMu, tanb, 91.1875,  1, mgut, False, slhafile.obj )
-print slhafile
+    slhafile = SLHAfile()
+    slhafile.ReadFile("")
+    r.lesHouchesAccordOutputStream( "sugra", inputs, sgnMu, tanb, 91.1875,  1, mgut, False, slhafile.obj )
+    #print slhafile
+ 
+if __name__=="__main__" :
+    from timeit import Timer
+    t = Timer( testPoint )
+    x = t.repeat(10,1)
+    print min(x)
