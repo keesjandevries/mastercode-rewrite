39089c596015e57d19a1388cedd890982374fa16
Sam Rogerson <samr@cern.ch>
2012-07-16 17:30:22 +0100

    tried to use COMPLEX: wont allow relocation b/c static libs

--- clean_rewrite/interfaces/feynhiggs.cc
+++ clean_rewrite/interfaces/feynhiggs.cc
@@ -1,14 +1,24 @@
 #include <iostream>
+#include <complex>
 
 #include "CFeynHiggs.h"
 #include "CSLHA.h"
 
 extern "C" {
-    void new_COMPLEX(double re, double im) {
-       return new COMPLEX(re,im); 
-    }
+    // this needs to be a c-interface that hides all of the implementation from python
+    // which sucks, can't pass out from the .a file so this is literally just
+    // going to have to be a "RUN", "GET VALUES" where it copies out hte values
+    // to we don't get relocate errors
 
-    void slharead(int *error, COMPLEX *slhadata, const char *filename, const int abort){
-        SLHARead(error,slhadata,filename,abort)
-    {
+    //std::complex<double>* COMPLEX_new(double re, double im) {
+       //return new std::complex<double>(re,im);
+    //}
+
+    //void slharead(int error, COMPLEX slhadata, const char filename,
+            //const int abort){
+        //SLHARead(&error,&slhadata,&filename,abort);
+    //}
+
+    void initFH(std::string slhafilename) {
+    }
 }
--- clean_rewrite/interfaces/feynhiggs.py
+++ clean_rewrite/interfaces/feynhiggs.py
@@ -3,27 +3,25 @@
 from ctypes import cdll, c_int, c_double, c_char_p, byref
 
 FHlib = cdll.LoadLibrary('./libs/libmcfeynhiggs.so')
+# const from FeynHiggs FIXME (src/inclue/SLHADefs.h)
 
-FHlib.foo()
+nslhadata = c_int(5558)
 
-#! /usr/bin/env python
+class FH_COMPLEX(object):
+    def __init__(self, re=0, img=0):
+        self.obj = FHlib.COMPLEX_new(c_double(re),c_double(img))
 
-# constant stolen from FeynHiggs... fortran wow...
-nslhadata = c_int(5426)
 
-def slharead( filename ) :
+def slharead(filename) :
     filename = "post_ss.slha"
 
     error = c_int(0)
-    slhadata = (c_double*nslhadata.value)()
+    slhadata = array( COMPLEX()*nslhadata.value)
     f_filename = c_char_p(filename)
     abort = c_int(1)
-
     f_len = c_int( len(filename) )
 
     # hidden argument at end fo len(f_filename)
     print "going to read this shit"
-    FHlib.slharead_( byref(error), slhadata, f_filename, byref(abort), f_len )
+    FHlib.slharead_( error, slhadata, f_filename, byref(abort), f_len )
     print "HNNNNN"
-
-(int *error, COMPLEX *slhadata, const char *filename, const int abort)
