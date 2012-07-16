f201c3b833b59dd8ad9425a3bf3395db94000147
Sam Rogerson <samr@cern.ch>
2012-07-16 13:05:47 +0100

    rename and update to compile FH

---
+++ clean_rewrite/compile.sh
@@ -0,0 +1,98 @@
+#! /bin/bash
+MAINDIR=`pwd`  
+SOFTSUSY_VERSION="3.3.1"
+SOFTSUSY_BASE="softsusy-${SOFTSUSY_VERSION}"
+SOFTSUSY_TARGET="http://www.hepforge.org/archive/softsusy/\
+${SOFTSUSY_BASE}.tar.gz"
+SOFTSUSY_LIB="packages/lib/libsoft.so"
+
+FEYNHIGGS_VERSION="2.9.1"
+FEYNHIGGS_BASE="FeynHiggs-${FEYNHIGGS_VERSION}"
+FEYNHIGGS_TARGET="http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/\
+newversion/${FEYNHIGGS_BASE}.tar.gz"
+FEYNHIGGS_LIB="packages/lib64/libFH.a"
+
+RFLAGS=`root-config --cflags --libs`
+
+
+function compile_softsusy {
+    echo -n "Compiling softsusy... "
+    if [[ ! -h ${SOFTSUSY_LIB} ]] || \
+       [[ `strings ${SOFTSUSY_LIB} | grep "${SOFTSUSY_VERSION}"` ]]; then
+        if [[ ! -f tars/${SOFTSUSY_BASE}.tar.gz ]]; then
+            wget -P tars/ ${SOFTUSY_TARGET}
+        fi
+        if [[ ! -d ${SOFTSUSY_BASE} ]]; then
+            tar zxf tars/${SOFTSUSY_BASE}.tar.gz
+        fi
+        cd ${SOFTSUSY_BASE}
+        ./configure --prefix=${MAINDIR}/packages
+        make
+        make install
+        cd ${MAINDIR}
+    fi
+    echo "Done"
+}
+
+
+function compile_feynhiggs {
+    echo -n "Compiling feynhiggs... "
+    if [[ ! -h ${FEYNHIGGS_LIB} ]] || \
+       [[ `strings ${FEYNHIGGS_LIB} | grep "${FEYNHIGGS_VERSION}"` ]]; then
+        if [[ ! -f tars/${FEYNHIGGS_BASE}.tar.gz ]]; then
+            wget -P tars/ ${FEYNHIGGS_TARGET}
+        fi
+        if [[ ! -d ${FEYNHIGGS_BASE} ]]; then
+            tar zxf tars/${FEYNHIGGS_BASE}.tar.gz
+        fi
+        cd ${FEYNHIGGS_BASE}
+        ./configure --prefix=${MAINDIR}/packages
+        make
+        make install
+        cd ${MAINDIR}
+    fi
+    echo "Done"
+}
+
+
+function compile_softsusy_interfaces {
+    g++ -c -fPIC -o obj/softsusy.o interfaces/softsusy.cc \
+        -I${MAINDIR}/packages/include/softsusy/ -L${MAINDIR}/packages/lib -lsoft
+    g++ -shared -Wl,-soname,libmcsoftsusy.so \
+        -Wl,-rpath,${MAINDIR}/packages/lib -o libs/libmcsoftsusy.so \
+       obj/softsusy.o -L${MAINDIR}/packages/lib -lsoft
+}
+
+
+function compile_slha_interfaces {
+    g++ -c -fPIC -o obj/slha.o interfaces/slha.cc \
+        -I${MAINDIR}/SLHA/inc/ -L${MAINDIR}/SLHA/libs -lSLHAfile \
+        ${RFLAGS}
+    g++ -shared -Wl,-soname,libmcslha.so \
+        -Wl,-rpath,${MAINDIR}/SLHA/libs -o libs/libmcslha.so \
+        obj/slha.o -L${MAINDIR}/SLHA/libs -lSLHAfile \
+        ${RFLAGS}
+}
+
+
+function compile_joint_interfaces {
+    # softsusy & slha join interface
+    g++ -c -fPIC -o obj/softsusy_slha.o interfaces/softsusy_slha.cc \
+        -I${MAINDIR}/SLHA/inc/ -L${MAINDIR}/SLHA/libs -lSLHAfile \
+        -I${MAINDIR}/packages/include/softsusy -L${MAINDIR}/packages/lib -lsoft \
+        ${RFLAGS}
+
+    g++ -shared -Wl,-soname,libmcsoftsusyslha.so \
+        -Wl,-rpath,${MAINDIR}/SLHA/libs:${MAINDIR}/packages/lib  \
+        -o libs/libmcsoftsusyslha.so obj/softsusy_slha.o \
+        -L${MAINDIR}/SLHA/libs -lSLHAfile -L${MAINDIR}/packages/lib -lsoft \
+        ${RFLAGS}
+}
+
+
+compile_softsusy
+compile_softsusy_interfaces
+compile_slha_interfaces
+compile_joint_interfaces
+
+compile_feynhiggs
