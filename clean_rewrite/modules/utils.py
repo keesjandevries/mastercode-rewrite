79f49304343a0dd6e1fb5c0a33ee1ac7fe486388
Sam Rogerson <samr@cern.ch>
2012-07-18 09:57:03 +0100

    function to pipe obj to function

---
+++ clean_rewrite/modules/utils.py
@@ -0,0 +1,25 @@
+import os
+
+def pipe_to_function(pipe_name, obj, function, debug=True):
+    try:
+        os.mkfifo(pipe_name)
+    except OSError, e:
+        print "Failed to create FIFO: %s" % e
+        exit()
+
+    child_pid = os.fork()
+    if child_pid != 0 :
+        f = open(pipe_name,'w')
+        f.write(str(obj))
+        f.close()
+    else:
+        if debug:
+            import sys
+            so = open("fh.stdout", 'w', 0)
+            se = open("fh.stderr", 'w', 0)
+            sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+            sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
+            os.dup2(so.fileno(), sys.stdout.fileno())
+            os.dup2(se.fileno(), sys.stderr.fileno())
+        function()
+        os._exit(0)
