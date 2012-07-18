61e838391888a92eeeeca6eb9b5d9e2e2c2eab0c
Sam Rogerson <samr@cern.ch>
2012-07-18 09:57:19 +0100

    use  new utils function

--- clean_rewrite/testPoint.py
+++ clean_rewrite/testPoint.py
@@ -8,8 +8,9 @@ from interfaces.softsusy import (DoubleVector, MssmSoftsusy, QedQcd)
 from interfaces.slha import SLHAfile
 from interfaces.feynhiggs import run_feynhiggs
 from modules import mcoutput
+from modules import utils
 
-def softsusy(m0, m12, A0, tanb, sgnMu, mgut, outputfile=None):
+def softsusy(m0, m12, A0, tanb, sgnMu, mgut):
     mcoutput.header('softsusy')
     inputs = DoubleVector(3)
     inputs[0] = m0
@@ -23,60 +24,35 @@ def softsusy(m0, m12, A0, tanb, sgnMu, mgut, outputfile=None):
     oneset.setMass(3,173.2)
 
     r.lowOrg( "sugraBcs", mgut, inputs, sgnMu, tanb, oneset, False )
-    if outputfile is None:
-        print "Printing to stdout"
-        r.lesHouchesAccordOutput( "sugra", inputs, sgnMu, tanb, 91.1875, 1,
-                mgut, False )
-    else:
-        print "Printing to slhafile"
-        slhafile = SLHAfile()
-        slhafile.ReadFile("")
 
-        r.lesHouchesAccordOutputStream( "sugra", inputs, sgnMu, tanb, 91.1875,
-                1, mgut, False, slhafile.obj )
+    slhafile = SLHAfile()
+    slhafile.ReadFile("")
 
-        f = open(outputfile,'w')
-        f.write(str(slhafile))
-        #print >> f, str(slhafile)
-        f.close()
+    #print "Printing to stdout"
+    #r.lesHouchesAccordOutput( "sugra", inputs, sgnMu, tanb, 91.1875, 1,
+            #mgut, False )
+
+    r.lesHouchesAccordOutputStream( "sugra", inputs, sgnMu, tanb, 91.1875,
+            1, mgut, False, slhafile.obj )
+
+    return slhafile
 
 
 def feynhiggs(filename):
     mcoutput.header('feynhiggs')
+    print "Fh looking for", filename
     run_feynhiggs(filename)
     return
 
 
 def testPoint() :
+    slhafile = softsusy(m0=100, m12=200, A0=0., tanb=10., sgnMu=1, mgut=2e16)
+
     t_now = strftime('%Y_%m_%d_%H_%M_%S', gmtime() )
     pipe_name = "/tmp/mc-{host}-{pid}-{time}".format(host=gethostname(),
             pid=os.getpid(), time=t_now)
-    try:
-        os.mkfifo(pipe_name)
-    except OSError, e:
-        print "Failed to create FIFO: %s" % e
-        exit()
 
-    child_pid = os.fork()
-    if child_pid != 0:
-        softsusy(m0=100, m12=200, A0=0., tanb=10., sgnMu=1, mgut=2e16,
-                outputfile=pipe_name)
-        p_pid,status = os.waitpid(child_pid,0)
-        print p_pid,status
-        os.unlink(pipe_name)
-    else:
-        import sys
-        so = open("fh.stdout", 'w', 0)
-        se = open("fh.stderr", 'w', 0)
-        sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-        sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
-        os.dup2(so.fileno(), sys.stdout.fileno())
-        os.dup2(se.fileno(), sys.stderr.fileno())
-
-        # this code does get executed but FH seems to die (imagine related to pipes)
-        feynhiggs(pipe_name)
-        print "IM LOLLING"
-        os.exit(0)
+    utils.pipe_to_function(pipe_name, slhafile, lambda: feynhiggs(pipe_name))
 
 
 if __name__=="__main__" :
