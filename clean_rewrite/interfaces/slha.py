baa9ca4517a8a7f550f842120dedb23ba6627932
Sam Rogerson <samr@cern.ch>
2012-07-18 14:37:14 +0100

    pass string buffer

--- clean_rewrite/interfaces/slha.py
+++ clean_rewrite/interfaces/slha.py
@@ -1,13 +1,18 @@
 #! /usr/bin/env python
 
-from ctypes import cdll
+_SLHAfile__MAX_SLHA_SIZE = 10000
+
+from ctypes import cdll, create_string_buffer
 SLHAlib = cdll.LoadLibrary('./libs/libmcslha.so')
 
 class SLHAfile(object):
     def __init__(self) :
         self.obj = SLHAlib.SLHAfile_new()
     def __str__(self) :
-       SLHAlib.SLHAfile_print( self.obj ) 
-       return ""
+        str = create_string_buffer(__MAX_SLHA_SIZE)
+        sz = SLHAlib.SLHAfile_getstr(self.obj, str, __MAX_SLHA_SIZE)
+        if sz >= __MAX_SLHA_SIZE:
+            print "*** WARNING: SLHA may be truncated"
+        return str.value
     def ReadFile(self, filename):
         SLHAlib.SLHAfile_ReadFile(self.obj, str(filename))
