cd0dcb0cc70c52d5de58185a2eede942c3ac0f73
Sam Rogerson <samr@cern.ch>
2012-07-24 16:04:39 +0100

    fetch urls and extract tarfiles

--- modules/utils.py
+++ modules/utils.py
@@ -1,5 +1,7 @@
 import os
 import time
+import urllib2
+import tarfile
 
 def pipe_to_function(pipe_name, obj, function):
     try:
@@ -20,3 +22,32 @@ def pipe_to_function(pipe_name, obj, function):
         os.unlink(pipe_name)
         print "CLOSED PIPE"
         os.waitpid(child_pid,0)
+
+def fetch_url(target, local_path):
+    success = False
+    try:
+        f = urllib2.urlopen(target)
+        print("Downloading {0} ...".format(target))
+        local_file = open(local_path,'wb')
+        local_file.write(f.read())
+        local_file.close()
+        print("  --> Done")
+        success = True
+    except urllib2.HTTPError, e:
+        print("HTTP Error:", e.code, target)
+    except urllib2.URLError, e:
+        print("URL Error:", e.reason, target)
+    return success
+
+def extract_tarfile(file, local_path):
+    print file, local_path
+    success = True
+    if tarfile.is_tarfile(file):
+        mode = 'r'
+        if file.endswith('.gz'):
+            mode += ':gz'
+        tf = tarfile.open(file,mode)
+        tf.extractall(local_path)
+        tf.close()
+    else:
+        success = False
