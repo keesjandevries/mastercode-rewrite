3612c6db8c25b78e8a7db3e0bfbe2a47d33054a7
Sam Rogerson <samr@cern.ch>
2012-07-24 17:05:57 +0100

    attempt to run configure scripts

--- compile.py
+++ compile.py
@@ -8,6 +8,7 @@ tar_dir = 'tars'
 predictor_dir = 'predictors'
 
 softsusy = {
+        'name': 'SoftSUSY',
         'version': '3.3.1',
         'source_url_fmt': 'http://www.hepforge.org/archive/softsusy/{0}',
         'source_filename': 'softsusy-3.3.1.tar.gz',
@@ -15,11 +16,13 @@ softsusy = {
         }
 
 feynhiggs = {
+        'name': 'FeynHiggs',
         'version': '2.9.1',
         'source_url_fmt': 'http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/'
                           'newversion/{0}',
         'source_filename': 'FeynHiggs-2.9.1.tar.gz',
-        'library': 'lib64/libFH.a'
+        'library': 'lib64/libFH.a',
+        'config_cd': True,
         }
 
 OPTIONS = {
@@ -55,14 +58,31 @@ def extract_predictors_source(predictors):
         if predictor['tar'] is not None:
             predictor['source_dir'] = extract_tarfile(predictor['tar'], predictor_dir)
 
-def compile_predictors(predictors):
+def compile_predictors(predictors, base_dir):
     for predictor in predictors:
+        try:
+            conf_dir = '{bd}/{pd}/'.format(bd=base_dir,
+                pd=predictor['source_dir'])
+            conf_file = '{cd}/configure'.format(cd=conf_dir)
+            with open(conf_file) as f: pass
+            print("Configuring {0} ...".format(predictor['name']))
+            prefix_str = '--prefix={bd}/{pd}'.format(bd=base_dir,
+                pd=prefix_dir)
+            if predictor.get('config_cd',False):
+                os.chdir(conf_dir)
+            subprocess.check_output([conf_file, prefix_str],
+                    stderr=subprocess.STDOUT)
+            os.chdir(base_dir)
+            print("  --> Done")
+        except IOError as e:
+            print("No config file present for {0}".format(predictor['name']))
 
 def main():
     predictors = OPTIONS['predictors']
+    base_dir = OPTIONS['basedir']
     get_predictors(predictors)
     extract_predictors_source(predictors)
-    compile_predictors(predictors)
+    compile_predictors(predictors, base_dir)
 
 if __name__=='__main__':
     main()
