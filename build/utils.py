f41ea5cbd7648d3f37bbc69568bfc2c286632b69
Sam Rogerson <samr@cern.ch>
2012-08-05 19:19:03 +0200

    build script for our utilities

---
+++ build/utils.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+slha_class = {
+    'source_dir': 'SLHA',
+    'name': 'SLHA class',
+    'installable': False,
+    }
+
+UTILS = [ slha_class ]
+
+
+def compile(base_dir):
+    for util in UTILS:
+        os.chdir('{bd}/{ud}'.format(bd=base_dir, ud=util['source_dir']))
+        print("Making {0} ...".format(util['name']))
+        subprocess.check_output(['make'], stderr=subprocess.STDOUT)
+        if util.get('installable', False):
+            subprocess.check_output(['make', 'install'],
+                    stderr=subprocess.STDOUT)
+        os.chdir(base_dir)
+        print("  --> Done")
+
+
+if __name__=='__main__':
+    compile(os.getcwd())
