2a023c01af0684838cce31451c9bd164d34d2bb3
Sam Rogerson <samr@cern.ch>
2012-08-07 13:59:05 +0100

    add slhablock

--- interfaces/slha.cc
+++ interfaces/slha.cc
@@ -1,6 +1,8 @@
 #include "SLHAfile.hh"
+#include "SLHAblock.hh"
 
 extern "C" {
+    //SLHA file
     SLHAfile* SLHAfile_new(){ 
         return new SLHAfile(); 
     }
@@ -22,6 +24,22 @@ extern "C" {
         std::string sf_str = ss_out.str();
         const char* lol_str = sf_str.c_str();
         strncpy(buf,lol_str,len-1);
+        buf[len-1]=0;
+        return strlen(buf);
+    }
+    
+    // SLHA block
+    SLHAblock* SLHAblock_new(const char* name){ 
+        return new SLHAblock(name); 
+    }
+
+    int SLHAblock_getstr(SLHAblock* sb, char* buf, int len) {
+        std::stringstream ss_out( std::stringstream::in |
+                                  std::stringstream::out );
+        ss_out << (*sb);
+        std::string bn_str = ss_out.str();
+        const char* sb_cstr = sb_str.c_str();
+        strncpy(buf,sb_cstr,len-1);
         buf[len-1]=0;
         return strlen(buf);
     }
--- interfaces/slha.py
+++ interfaces/slha.py
@@ -6,13 +6,23 @@ from ctypes import cdll, create_string_buffer
 SLHAlib = cdll.LoadLibrary('./libs/libmcslha.so')
 
 class SLHAfile(object):
-    def __init__(self) :
+    def __init__(self):
         self.obj = SLHAlib.SLHAfile_new()
-    def __str__(self) :
-        str = create_string_buffer(__MAX_SLHA_SIZE)
-        sz = SLHAlib.SLHAfile_getstr(self.obj, str, __MAX_SLHA_SIZE)
+    def __str__(self):
+        slha_str = create_string_buffer(__MAX_SLHA_SIZE)
+        sz = SLHAlib.SLHAfile_getstr(self.obj, slha_str, __MAX_SLHA_SIZE)
         if sz >= __MAX_SLHA_SIZE:
-            print "*** WARNING: SLHA may be truncated"
-        return str.value
+            print "*** WARNING: SLHA file may be truncated"
+        return slha_str.value
     def ReadFile(self, filename):
         SLHAlib.SLHAfile_ReadFile(self.obj, str(filename))
+
+class SLHAblock(object):
+    def __init__(self, name):
+        self.obj = SLHAlib.SLHAblock_new(str(name))
+    def __str__(self):
+        block_str = create_string_buffer(__MAX_SLHA_SIZE)
+        sz = SLHAlib.SLHAblock_getstr(self.obj, block_Str, __MAX_SLHA_SIZE)
+        if sz >= __MAX_SLHA_SIZE:
+            print "*** WARNING: SLHA block may be truncated"
+        return block_str.value
