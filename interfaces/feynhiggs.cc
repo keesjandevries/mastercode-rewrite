24df57a35e1551e7735868db9f47a5305b9c4e8c
Sam Rogerson <samr@cern.ch>
2012-09-06 15:02:09 +0100

    remove prec obs arrayfor now

--- interfaces/feynhiggs.cc
+++ interfaces/feynhiggs.cc
@@ -14,7 +14,7 @@ const bool WRITE_SLHA = false;
 extern "C" {
     void run_feynhiggs(const char slhafilename [], int mssmpart, int fieldren,
             int tanbren, int higgsmix, int p2approx, int looplevel,
-            int tl_running_mt, int tl_bot_resum, double* prec_obs_array) {
+            int tl_running_mt, int tl_bot_resum) {
 
         COMPLEX slhadata[fh_interface_nslhadata]; // stupid typedefs: not a true constructor
         int error;
@@ -51,21 +51,21 @@ extern "C" {
                 SLHAWrite(&error, slhadata, slha_name);
                 std::cout << "Wrote FH SLHA" << std::endl;
             }
-            prec_obs_array[0] = PrecObs_DeltaRho;
-            prec_obs_array[1] = PrecObs_MWMSSM;
-            prec_obs_array[2] = PrecObs_MWSM;
-            prec_obs_array[3] = PrecObs_SW2effMSSM;
-            prec_obs_array[4] = PrecObs_SW2effSM;
-            prec_obs_array[5] = PrecObs_gminus2mu;
-            prec_obs_array[6] = PrecObs_EDMeTh;
-            prec_obs_array[7] = PrecObs_EDMn;
-            prec_obs_array[8] = PrecObs_EDMHg;
-            prec_obs_array[9] = PrecObs_bsgammaMSSM;
-            prec_obs_array[10] = PrecObs_bsgammaSM;
-            prec_obs_array[11] = PrecObs_DeltaMsMSSM;
-            prec_obs_array[12] = PrecObs_DeltaMsSM;
-            prec_obs_array[13] = PrecObs_BsmumuMSSM;
-            prec_obs_array[14] = PrecObs_BsmumuSM;
+            //prec_obs_array[0] = PrecObs_DeltaRho;
+            //prec_obs_array[1] = PrecObs_MWMSSM;
+            //prec_obs_array[2] = PrecObs_MWSM;
+            //prec_obs_array[3] = PrecObs_SW2effMSSM;
+            //prec_obs_array[4] = PrecObs_SW2effSM;
+            //prec_obs_array[5] = PrecObs_gminus2mu;
+            //prec_obs_array[6] = PrecObs_EDMeTh;
+            //prec_obs_array[7] = PrecObs_EDMn;
+            //prec_obs_array[8] = PrecObs_EDMHg;
+            //prec_obs_array[9] = PrecObs_bsgammaMSSM;
+            //prec_obs_array[10] = PrecObs_bsgammaSM;
+            //prec_obs_array[11] = PrecObs_DeltaMsMSSM;
+            //prec_obs_array[12] = PrecObs_DeltaMsSM;
+            //prec_obs_array[13] = PrecObs_BsmumuMSSM;
+            //prec_obs_array[14] = PrecObs_BsmumuSM;
         }
     }
 }
