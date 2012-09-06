803d6c43ec8ce3054b119e42ba8992d8d858ff3e
Sam Rogerson <samr@cern.ch>
2012-09-06 09:37:44 +0100

    rge calc naming

--- testPoint.py
+++ testPoint.py
@@ -4,13 +4,13 @@ import os
 from socket import gethostname
 from time import gmtime, strftime
 
-from interfaces import softsusy
+from interfaces import softsusy as rge_calc
 from interfaces import feynhiggs
 from modules import utils
 
 
 def run_point(tanb, sgnMu, mgut, mt, boundary_condition, vars) :
-    slhafile = softsusy.run(tanb, sgnMu, mgut, mt, boundary_condition, vars)
+    slhafile = rge_calc.run(tanb, sgnMu, mgut, mt, boundary_condition, vars)
 
     t_now = strftime('%Y_%m_%d_%H_%M_%S', gmtime() )
     pipe_name = "/tmp/mc-{host}-{pid}-{time}".format(host=gethostname(),
@@ -18,6 +18,8 @@ def run_point(tanb, sgnMu, mgut, mt, boundary_condition, vars) :
 
     utils.pipe_to_function(pipe_name, slhafile,
             lambda: feynhiggs.run(pipe_name))
+
+    print slhafile
 
 
 if __name__=="__main__" :
