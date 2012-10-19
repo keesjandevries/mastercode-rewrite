27f61a3dbd34a141584a66d5b30dedf8ec31f85b
Sam Rogerson <samr@cern.ch>
2012-10-19 04:20:55 +0100

    check for rootsys

--- compile.py
+++ compile.py
@@ -7,5 +7,9 @@ OPTIONS = {
         'base_dir': os.getcwd(),
         }
 
+#root_flags = subprocess.check_output(['root-config','--cflags','--libs'])
+if not os.environ.get('ROOTSYS',None):
+    raise OSError("ROOTSYS not defined: please source ROOT before building")
+
 for mod in [ predictors, utils ]:
     mod.compile(**OPTIONS)
