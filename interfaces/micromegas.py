effda20ba85c97d6f696fa529aead38a78d4354b
Sam Rogerson <samr@cern.ch>
2012-10-22 17:01:13 +0100

    remove unused vars

--- interfaces/micromegas.py
+++ interfaces/micromegas.py
@@ -6,15 +6,6 @@ from modules import mcoutput
 
 MOlib = cdll.LoadLibrary('./libs/libmcmicromegas.so')
 
-mssmpart = 4
-fieldren = 0
-tanbren = 0
-higgsmix = 2
-p2approx = 0
-looplevel = 2
-Tl_mt = 1
-tl_bot_resum = 1
-
 class MicromegasPrecObs(Structure):
     _fields_ = [('Omega', c_double), ('Bll', c_double), ('Bsg', c_double),
             ('SMbsg', c_double)]
