e3d11b722dea0262a193cc190de85849466e14c0
Sam Rogerson <samr@cern.ch>
2012-11-03 21:16:32 +0000

    attempt at generic model interface

--- testPoint.py
+++ testPoint.py
@@ -3,24 +3,17 @@ import os
 
 from collections import OrderedDict
 
-from interfaces import softsusy as rge_calc
+from interfaces import softsusy as slha_generator
 from interfaces import feynhiggs, micromegas, superiso
 from modules import utils
 
 from interfaces import slha
 
-models = {
-        'cMSSM': {
-            'generator': ['softsusy'][0],
-            'inputs': ['m0', 'm12', 'a0', 'tanb', 'sgnMu'],
-            'fixed_vars': ['mgut', 'mt'],
-            }
-        }
-
 predictors = [feynhiggs, micromegas, superiso]
 
-def run_point(tanb, sgnMu, mgut, mt, model, i_vars) :
-    slhafile = rge_calc.run(tanb, sgnMu, mgut, mt, model, i_vars)
+#def run_point(tanb, sgnMu, mgut, mt, model, i_vars) :
+def run_point(model, **inputs):
+    slhafile = slha_generator.run(model, **inputs)
     #slha.process_slhafile(slhafile)
     pipe_name = "/tmp/mc-{u}".format(u=utils.unique_str())
 
@@ -40,7 +33,7 @@ def run_point(tanb, sgnMu, mgut, mt, model, i_vars) :
 
 if __name__=="__main__" :
     model = 'cMSSM'
-    i_vars = [ 100, 200, 0 ]
+    i_vars = { 'm0': 100, 'm12': 200, 'A0': 0 }
     boundary_condition = "sugraBcs"
     run_point(tanb=10., sgnMu=1, mgut=2e16, mt=173.2,
-            model=model, i_vars=i_vars)
+            model=model, **i_vars)
