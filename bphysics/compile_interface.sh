21ef02dee8653f1db38ac192a158d9f1fb98bcac
Sam Rogerson <samr@cern.ch>
2012-11-08 12:55:30 +0000

    gfortran

--- bphysics/compile_interface.sh
+++ bphysics/compile_interface.sh
@@ -4,6 +4,9 @@ SLHALIB=/home/hep/kjd110/mastercode-rewrite/SLHALib-2.2/x86_64-linux/
 FCC=/home/hep/kjd110/mastercode-rewrite/SLHALib-2.2/x86_64-linux/../build/fcc
 
 g++  -c  -o interface.o  bphysics.cc -I$SLHALIB/include 
-g77 -c  -o bphysics.o  bphysics.F -ffixed-line-length-none -I$SLHALIB/include 
+gfortran -c  -o bphysics.o  bphysics.F -ffixed-line-length-none -I$SLHALIB/include 
 
-g++ -o interface  -I$SLHALIB/include interface.o bphysics.o     -L$SLHALIB/lib -lSLHA   -lpthread -L/usr/lib/gcc/x86_64-redhat-linux/3.4.6 -L/usr/lib/gcc/x86_64-redhat-linux/3.4.6 -L/usr/lib/gcc/x86_64-redhat-linux/3.4.6/../../../../lib64 -L/usr/lib/gcc/x86_64-redhat-linux/3.4.6/../../.. -L/lib/../lib64 -L/usr/lib/../lib64 -lfrtbegin -lg2c -lm -lgcc_s -lgcc_s
+g++ -o interface interface.o bphysics.o 
+    -I$SLHALIB/include \
+    -L$SLHALIB/lib -lSLHA \
+    -lpthread -lfrtbegin -lgfortran -lm -lgcc_s -lgcc_s
