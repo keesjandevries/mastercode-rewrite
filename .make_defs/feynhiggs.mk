1717e33b8836f71abdda665594e6b5ac70204075
Sam Rogerson <samr@cern.ch>
2012-11-14 16:51:19 +0000

    add definitions for feynhiggs

---
+++ .make_defs/feynhiggs.mk
@@ -0,0 +1,14 @@
+version=2.9.4
+name=FeynHiggs-$(version)
+src_dir=$(name)
+machine=$(shell uname -m)
+ifeq ($(machine), x86_64)
+	lib_dir=lib64
+else
+	lib_dir=lib
+endif
+lib=$(INSTALL_DIR)/$(lib_dir)/libFH.a
+tar_name=$(name).tar.gz
+remote_url=http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/newversion/
+remote=$(remote_url)/$(tar_name)
+tarfile=$(TAR_DIR)/$(tar_name)
