ed6c7bfdfe5cd57f3b3640de3ed1a5ff331e1722
Sam Rogerson <samr@cern.ch>
2012-11-14 21:18:49 +0000

    p1

--- .make/feynhiggs_interface.mk
+++ .make/feynhiggs_interface.mk
@@ -1,17 +1,4 @@
-version=2.9.4
-name=FeynHiggs-$(version)
-src_dir=$(name)
-machine=$(shell uname -m)
-ifeq ($(machine), x86_64)
-	lib_dir=lib64
-else
-	lib_dir=lib
-endif
-lib=$(INSTALL_DIR)/$(lib_dir)/libFH.a
-tar_name=$(name).tar.gz
-remote_url=http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/newversion/
-remote=$(remote_url)/$(tar_name)
-tarfile=$(TAR_DIR)/$(tar_name)
+include $(DEF_DIR)/feynhiggs.mk
 
 $(lib):
 ifeq ($(wildcard $(tarfile)),)
@@ -20,7 +7,7 @@ endif
 ifeq ($(wildcard $(src_dir)),)
 	tar -xf $(tarfile)
 endif
-	-patch -N -p2 -i $(PATCH_DIR)/FeynHiggs.patch
+	-patch -N -p1 -i $(PATCH_DIR)/FeynHiggs.patch
 	cd $(src_dir); \
 		./configure --prefix=$(INSTALL_DIR);
 	$(MAKE) -C $(src_dir)
