abc67dfe6ad8c5c4b41798d1cfe927cd798af4e5
Sam Rogerson <samr@cern.ch>
2012-11-19 12:50:45 +0000

    remove old slha format

--- .make_defs/slhaclass.mk
+++
@@ -1,8 +0,0 @@
-src_dir=$(UTIL_DIR)/SLHA
-lib_dir=$(src_dir)/libs/
-lib=$(lib_dir)/libSLHAfile.so
-lib_short=SLHAfile
-interface_src=$(INTERFACE_DIR)/slhaclass.cc
-interface_lib=$(LIB_DIR)/libmcslhaclass.so
-
-cc=g++
