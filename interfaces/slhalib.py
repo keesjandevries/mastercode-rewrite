20db52715e771036bc7c19315de19593af593b3f
Sam Rogerson <samr@cern.ch>
2012-11-19 01:13:34 +0000

    c_complex -> utils

--- interfaces/slhalib.py
+++ interfaces/slhalib.py
@@ -2,6 +2,7 @@
 
 from ctypes import cdll, c_int, c_double, c_char_p, byref, Structure
 from collections import OrderedDict
+from module.utils import c_complex
 
 name = "FeynHiggs"
 SLlib = cdll.LoadLibrary('packages/lib/libmcslhalib.so')
@@ -10,11 +11,8 @@ nslhadata = SLlib.get_nslhadata()
 # this idea *requires* that the memory allocation of a complex type (C99, c++0x)
 # is the same as an array of 2 doubles.  This is in the standard, but mileage
 # with different compilers may vary
-class Complex(Structure):
-    _fields_ = [('re', c_double), ('im', c_double)]
-
 class SLHAData(Structure):
-    _fields_ = [('carray', Complex * nslhadata)]
+    _fields_ = [('carray', c_complex * nslhadata)]
 
 def read(filename):
     data = SLHAData()
