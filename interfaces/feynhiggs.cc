dfc3c383f30f4181656f37e4129b3b012f65ac37
Sam Rogerson <samr@cern.ch>
2012-11-19 03:01:47 +0000

    update slha values after higgscorr and constraints

--- interfaces/feynhiggs.cc
+++ interfaces/feynhiggs.cc
@@ -2,7 +2,8 @@
 #include <complex>
 #include <string>
 #include <fstream>
-#include <stdlib.h>
+#include <cmath>
+#include <cstdlib>
 
 #include "CFeynHiggs.h"
 #include "CSLHA.h"
@@ -19,7 +20,7 @@ struct FeynHiggsPrecObs {
     //double gm2, DeltaRho, MWMSSM, MWSM, SW2effMSSM, SW2effSM, EDMeTh,
            //EDMn, EDMHg, bsgammaMSSM, bsgammaSM, DeltaMsMSSM, DeltaMsSM,
            //BsmumuMSSM, BsmumuSM;
-    double  gm2, DeltaRho, MWMSSM, MWSM, SW2MSSM, SW2SM, edmeTh, edmn, edmHg;
+    double gm2, DeltaRho, MWMSSM, MWSM, SW2MSSM, SW2SM, edmeTh, edmn, edmHg;
     double mh, mH, mA, mHpm;
 };
 
@@ -54,15 +55,10 @@ extern "C" {
         }
 
         double mhiggs[4];
-        ComplexType SAeff;
-        ComplexType UHiggs[3][3];
-        ComplexType ZHiggs[3][3];
+        std::complex<double> SAeff;
+        std::complex<double> UHiggs[3][3];
+        std::complex<double> ZHiggs[3][3];
         FHHiggsCorr(&error, mhiggs, &SAeff, UHiggs, ZHiggs);
-
-        out->mh = mhiggs[0];
-        out->mH = mhiggs[1];
-        out->mA = mhiggs[2];
-        out->mHpm = mhiggs[3];
 
         int ccb;
 
@@ -75,6 +71,36 @@ extern "C" {
             // FH has failed
         }
         else {
+            // update higgs values
+            out->mh = mhiggs[0];
+            out->mH = mhiggs[1];
+            out->mA = mhiggs[2];
+            out->mHpm = mhiggs[3];
+
+            Mass_Mh0.re = mhiggs[0];
+            Mass_MHH.re = mhiggs[1];
+            Mass_MA0.re = mhiggs[2];
+            Mass_MHp.re = mhiggs[3];
+            for(int i=1; i<4; ++i) {
+                for( int j=1; j<4; ++j) {
+                    CVHMix_UH(i,j).re = std::real(UHiggs[i][j]);
+                }
+            }
+            Alpha_Alpha.re = asin(std::real(SAeff));
+
+            // update precobs
+            PrecObs_gminus2mu.re = out->gm2;
+            PrecObs_DeltaRho.re  = out->DeltaRho;
+            PrecObs_MWSM.re      = out->MWSM;
+            PrecObs_SW2effSM.re  = out->SW2SM;
+            PrecObs_EDMeTh.re    = out->edmeTh;
+            PrecObs_EDMn.re      = out->edmn;
+            PrecObs_EDMHg.re     = out->edmHg;
+            //PrecObs_MW.re        = out->MWMSSM;
+            //PrecObs_SW2eff.re    = out->SW2MSSM;
+            //PrecObs_bsgamma.re   = out->bsgammaMSSM;
+            //PrecObs_bsgammaSM.re = out->bsgammaSM;
+
             std::cout << "FH SUCCESS" << std::endl;
             if( write_fh_slha ) {
                 const char fh_slha_name[] = "slhas/feyn_out.slha";
@@ -82,22 +108,6 @@ extern "C" {
                 SLHAWrite(&error, slhadata, fh_slha_name);
                 std::cout << "Wrote FH SLHA" << std::endl;
             }
-            // FIXME: these dont get filled
-            //out->DeltaRho = PrecObs_DeltaRho.re;
-            //out->MWMSSM = PrecObs_MWMSSM.re;
-            //out->MWSM = PrecObs_MWSM.re;
-            //out->SW2effMSSM = PrecObs_SW2effMSSM.re;
-            //out->SW2effSM = PrecObs_SW2effSM.re;
-            //out->gm2 = PrecObs_gminus2mu.re;
-            //out->EDMeTh = PrecObs_EDMeTh.re;
-            //out->EDMn = PrecObs_EDMn.re;
-            //out->EDMHg = PrecObs_EDMHg.re;
-            //out->bsgammaMSSM = PrecObs_bsgammaMSSM.re;
-            //out->bsgammaSM = PrecObs_bsgammaSM.re;
-            //out->DeltaMsMSSM = PrecObs_DeltaMsMSSM.re;
-            //out->DeltaMsSM = PrecObs_DeltaMsSM.re;
-            //out->BsmumuMSSM = PrecObs_BsmumuMSSM.re;
-            //out->BsmumuSM = PrecObs_BsmumuSM.re;
         }
     }
 }
