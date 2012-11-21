4402b5bf12812924669fbf72b20709f694dec1d4
Sam Rogerson <samr@cern.ch>
2012-11-21 16:12:14 +0000

    add run and fix name

--- interfaces/slhalib.py
+++ interfaces/slhalib.py
@@ -4,7 +4,7 @@ from ctypes import cdll, c_int, c_double, c_char_p, byref, Structure
 from collections import OrderedDict
 from modules.utils import c_complex, pipe_object_to_function, unique_str
 
-name = "FeynHiggs"
+name = "SLHALib"
 SLlib = cdll.LoadLibrary('packages/lib/libmcslhalib.so')
 nslhadata = SLlib.get_nslhadata()
 
@@ -42,3 +42,9 @@ class SLHA(object):
 
 def send_to_predictor(slhadata, predictor, update=False):
     return predictor.run(slhadata, update)
+
+def run(*args, **kwargs):
+    assert "filename" in kwargs
+    slha = SLHA()
+    slha.read(kwargs[filename])
+    return slha
