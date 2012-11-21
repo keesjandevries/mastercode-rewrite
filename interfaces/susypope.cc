2c59640dc4787e19edd342f4796f73ba0cdeda7f
Sam Rogerson <samr@cern.ch>
2012-11-21 17:35:58 +0000

    remove unnecessary class

--- interfaces/susypope.cc
+++ interfaces/susypope.cc
@@ -16,7 +16,6 @@
 struct susypopeFlags{
     int LoopOption, IterOpt, Observables, HiggsOpt, Verbose,  SMObsOpt;
     // constructors
-    susypopeFlags(){}
     susypopeFlags(int lOpt, int iOpt, int obs, int hOpt, int v, int smOpt):
         LoopOption(lOpt), IterOpt(iOpt), Observables(obs), HiggsOpt(hOpt),
         Verbose(v), SMObsOpt(smOpt){}
@@ -30,11 +29,6 @@ struct susypopeNoneSLHA {
 };
 
 // combination of flags and non-slha inputs
-struct susypopeIn {
-    susypopeNoneSLHA n_slha;
-    susypopeFlags flags;
-};
-
 // output
 struct susypopeObs {
     double MSSMObs[35], SMObs[35];
@@ -154,15 +148,15 @@ int set_flags(susypopeFlags * flags){
 
 extern "C" {
 
-    void run_susypope(std::complex<double>* slhadata, susypopeIn* in,
-            susypopeObs* out) {
-        out->SMObs[0];
+    void run_susypope(std::complex<double>* slhadata, susypopeNoneSLHA* n_slha,
+        susypopeFlags* flags, susypopeObs* out) {
         int error(0);
-        error = set_flags(&(in->flags));
+        error = set_flags(flags);
         if (error) std::cout << "set flags failed" << std::endl;
-        error = set_parameters(slhadata, &in->n_slha);
+        error = set_parameters(slhadata, n_slha);
         if (error) std::cout << "set parameters failed" << std::endl;
         calcobs_amw_(error, out->MSSMObs, out->SMObs);
+        if (error) std::cout << "calcobs failed" << std::endl;
 
         // extract observables in more readable terms
         out->MW             =  out->SMObs[0 ];
@@ -185,49 +179,45 @@ extern "C" {
 
 
 int main() {
-    susypopeObs  obs; 
-    susypopeObs * pobs = &obs;
-    susypopeIn inp;
-    susypopeIn * pinp = &inp;
-//    inp.gammaZ=2.4952;
-//    inp.alphaHad=0.02749;
-    susypopeFlags opts(5,1,1,1,1,1);
-    inp.flags=opts;
-    inp.n_slha.DeltaAlfa5had = 0.02749;
-    inp.n_slha.DeltaAlfaQED  = 0.031497637;
-    inp.n_slha.ZWidthexp     = 2.4952;
+    susypopeObs obs; 
+    susypopeFlags flags(5,1,1,1,1,1);
+    susypopeNoneSLHA n_slha;
 
-    inp.n_slha.M2phase   = 0.0;
-    inp.n_slha.M1phase   = 0.0;
-    inp.n_slha.MUEPhase  = 0.0;
-    inp.n_slha.Atphase   = 0.0;
-    inp.n_slha.Abphase   = 0.0;
-    inp.n_slha.Atauphase = 0.0;
+    n_slha.DeltaAlfa5had = 0.02749;
+    n_slha.DeltaAlfaQED  = 0.031497637;
+    n_slha.ZWidthexp     = 2.4952;
 
-    inp.n_slha.MB   = 4.8;
+    n_slha.M2phase   = 0.0;
+    n_slha.M1phase   = 0.0;
+    n_slha.MUEPhase  = 0.0;
+    n_slha.Atphase   = 0.0;
+    n_slha.Abphase   = 0.0;
+    n_slha.Atauphase = 0.0;
 
-    char slhaname[]="../slhas/test.slha";
+    n_slha.MB   = 4.8;
+
+    char slhaname[]="slhas/sp_test.slha";
     // read the slha file
     int error=0;
     std::complex<double>  slhadata[nslhadata];
     const int abort(0);
     SLHARead(&error, slhadata, slhaname , abort);
 
-    run_susypope(slhadata,pinp,pobs);
+    run_susypope(slhadata,&n_slha,&flags,&obs);
 
-    std::cout <<  "MW           : " <<  pobs->SMObs[0]      << std::endl ;
-    std::cout <<  "sin_theta_eff: " <<  pobs->sin_theta_eff << std::endl ;
-    std::cout <<  "Gamma_z      : " <<  pobs->Gamma_z       << std::endl ;
-    std::cout <<  "Rl           : " <<  pobs->Rl            << std::endl ;
-    std::cout <<  "Rb           : " <<  pobs->Rb            << std::endl ;
-    std::cout <<  "Rc           : " <<  pobs->Rc            << std::endl ;
-    std::cout <<  "Afb_b        : " <<  pobs->Afb_b         << std::endl ;
-    std::cout <<  "Afb_c        : " <<  pobs->Afb_c         << std::endl ;
-    std::cout <<  "Ab_16        : " <<  pobs->Ab_16         << std::endl ;
-    std::cout <<  "Ac_17        : " <<  pobs->Ac_17         << std::endl ;
-    std::cout <<  "Al           : " <<  pobs->Al            << std::endl ;
-    std::cout <<  "Al_fb        : " <<  pobs->Al_fb         << std::endl ;
-    std::cout <<  "sigma_had    : " <<  pobs->sigma_had     << std::endl ;
+    std::cout <<  "MW           : " <<  obs.SMObs[0]      << std::endl ;
+    std::cout <<  "sin_theta_eff: " <<  obs.sin_theta_eff << std::endl ;
+    std::cout <<  "Gamma_z      : " <<  obs.Gamma_z       << std::endl ;
+    std::cout <<  "Rl           : " <<  obs.Rl            << std::endl ;
+    std::cout <<  "Rb           : " <<  obs.Rb            << std::endl ;
+    std::cout <<  "Rc           : " <<  obs.Rc            << std::endl ;
+    std::cout <<  "Afb_b        : " <<  obs.Afb_b         << std::endl ;
+    std::cout <<  "Afb_c        : " <<  obs.Afb_c         << std::endl ;
+    std::cout <<  "Ab_16        : " <<  obs.Ab_16         << std::endl ;
+    std::cout <<  "Ac_17        : " <<  obs.Ac_17         << std::endl ;
+    std::cout <<  "Al           : " <<  obs.Al            << std::endl ;
+    std::cout <<  "Al_fb        : " <<  obs.Al_fb         << std::endl ;
+    std::cout <<  "sigma_had    : " <<  obs.sigma_had     << std::endl ;
 
 }
 
