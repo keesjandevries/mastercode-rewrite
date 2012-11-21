94e9bfef262f7f5bbd713308ebe2efed9977f21e
Sam Rogerson <samr@cern.ch>
2012-11-21 17:35:38 +0000

    inits for structs

--- interfaces/susypope.py
+++ interfaces/susypope.py
@@ -1,33 +1,27 @@
 #! /usr/bin/env python
 
-from ctypes import cdll, c_int, c_double, c_char_p, byref, Structure
+from ctypes import cdll, c_int, c_double, byref, Structure
 from collections import OrderedDict
-
-from modules.utils import c_complex
 
 name = "SUSY-POPE"
 SPlib = cdll.LoadLibrary('packages/lib/libmcsusypope.so')
-
-default_gammaZ = 2.4952
-default_alphaHad = 0.02749
-
-    # DEFAULTS
-    #susypopeFlags opts(5,1,1,1,1,1);
-    #inp.flags=opts;
-    #inp.n_slha.DeltaAlfa5had = 0.02749;
-    #inp.n_slha.DeltaAlfaQED  = 0.031497637;
-    #inp.n_slha.ZWidthexp     = 2.4952;
-
-class SUSYPOPEOpts(Structure):
-    _fields_ = [('gammaZ', c_double), ('alphaHad', c_double)]
-    def __init__(self, gammaZ, alphaHad):
-        self.gammaZ = gammaZ
-        self.alphaHad = alphaHad
 
 class susypopeFlags(Structure):
     _fields_ = [('LoopOption', c_int), ('IterOpt', c_int),
             ('Observables', c_int), ('HiggsOpt', c_int), ('Verbose', c_int),
             ('SMObsOpt', c_int)]
+    def __init__(self, flags=None):
+        if flags is None:
+            self.LoopOption = 5
+            self.IterOpt = 1
+            self.Observables = 1
+            self.HiggsOpt = 1
+            self.Verbose = 1
+            self.SMObsOpt = 1
+        else:
+            for attr, value in flags.iteritems():
+                if attr in self.__dict__:
+                    setattr(self,attr,value)
 
 class susypopeNoneSLHA(Structure):
     _fields_ = [('DeltaAlfa5had', c_double), ('DeltaAlfaQED', c_double),
@@ -35,20 +29,19 @@ class susypopeNoneSLHA(Structure):
             ('M1phase', c_double), ('MUEPhase', c_double),
             ('Atphase', c_double), ('Abphase', c_double),
             ('Atauphase', c_double), ('MB', c_double)]
+    def __init__(self, flags=None):
+        if flags is None:
+            self.DeltaAlfa5had = 0.02749
+            self.DeltaAlfaQED = 0.031497637
+            self.ZWidthexp = 2.4952
+            self.MB = 4.8
+        else:
+            for attr, value in flags.iteritems():
+                if attr in self.__dict__:
+                    setattr(self,attr,value)
 
-class susypopeIn(Structure):
-    _fields_ = [('n_slha', susypopeNoneSLHA), ('flags', susypopeFlags)]
-
-
+# output
 class susypopeObs(Structure):
-    _fields_ = [('MSSMObs', c_double*35), ('SMObs', c_double*35),
-        ('MW', c_double), ('sin_theta_eff', c_double), ('Gamma_z', c_double),
-        ('Rl', c_double), ('Rb', c_double), ('Rc', c_double),
-        ('Afb_b', c_double), ('Afb_c', c_double), ('Ab_16', c_double),
-        ('Ac_17', c_double), ('Al', c_double), ('Al_fb', c_double),
-        ('sigma_had', c_double)]
-
-class SUSYPOPEObs(Structure):
     _fields_ = [('MSSMObs', c_double*35), ('SMObs', c_double*35),
             ('MW', c_double), ('sin_theta_eff', c_double),
             ('Gamma_z', c_double), ('Rl', c_double), ('Rb', c_double),
@@ -56,9 +49,10 @@ class SUSYPOPEObs(Structure):
             ('Ab_16', c_double), ('Ac_17', c_double), ('Al', c_double),
             ('Al_fb', c_double), ('sigma_had', c_double)]
 
-def run(slhadata, update, spopts=None):
-    if spopts is None:
-        spopts = SUSYPOPEOpts(gammaZ=2.4952, alphaHad=0.02749)
 
-    spout = SUSYPOPEObs()
-    SPlib.run_susypope(byref(slhadata.data), byref(spopts), byref(SPout))
+def run(slhadata, update=False):
+    n_slha = susypopeNoneSLHA()
+    flags = susypopeFlags()
+    spout = susypopeObs()
+    SPlib.run_susypope(byref(slhadata.data), byref(n_slha), byref(flags),
+            byref(spout))
