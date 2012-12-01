eeb4af29e0975b7774d71a5f8b19c84575816369
Sam Rogerson <samr@cern.ch>
2012-12-01 18:36:19 +0000

    32bit for those that link against SLHALib

--- .make/lspscat_interface.mk
+++ .make/lspscat_interface.mk
@@ -7,13 +7,13 @@ lspscat_interface: $(interface_lib)
 interface_obj=$(interface_src:.cc=.o)
 
 $(interface_lib): $(interface_obj)
-	$(cc) -shared -Wl,-soname,libmclspscat.so -o $(interface_lib) \
+	$(cc) -m32 -shared -Wl,-soname,libmclspscat.so -o $(interface_lib) \
 		-Wl,-rpath,$(LIB_DIR) \
 		$(interface_obj) $(obj) -L$(LIB_DIR) -lSLHA \
 		-lgfortran
 
 $(interface_obj):
-	$(cc) -c -fPIC -o $(interface_obj) $(interface_src) \
+	$(cc) -m32 -c -fPIC -o $(interface_obj) $(interface_src) \
 		-I$(INCLUDE_DIR)/SLHALib
 
 clean:
