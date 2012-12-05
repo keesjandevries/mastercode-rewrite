a09d4244b75f3ef8b34e76d381d5a4ec4d1ba708
Sam Rogerson <samr@cern.ch>
2012-12-05 16:21:02 +0000

    more cleanup

--- run_point.py
+++ run_point.py
@@ -51,7 +51,7 @@ def run_point(model, **inputs):
             if type(value) is not list:
                 print f_str.format(n=name, p=value)
             else:
-                print f_str.format(n=name, p="[{0},...{1}][{2}]".format(
+                print f_str.format(n=name, p="[{0}, ... , {1}][{2}]".format(
                     value[0],value[-1],len(value)))
 
     #print slhafile
