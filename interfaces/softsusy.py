d4115c9b9a67c1cec7a6210709ef6196ded0a553
Keesjan de Vries <keesjan777@gmail.com>
2012-12-07 13:12:53 +0000

    add setup functions to allow MssmSoftsusy attribute setting per model

--- interfaces/softsusy.py
+++ interfaces/softsusy.py
@@ -21,6 +21,9 @@ models = {
                 'gaugeUnification': False,
                 'ewsbBCscale': False,
                 },
+            'other_setup':  {
+                'setMixing': [0],
+                }
             },
         'pMSSM': {
             'universals': [('M_1', 1), ('M_2', 2), ('M_3', 3), ('At', 11),
@@ -35,6 +38,10 @@ models = {
             'other_vars': {
                 'gaugeUnification': False,
                 'ewsbBCscale': True,
+                },
+            'other_setup':  {
+                #'setSetTbAtMX': [True],
+                'setMixing': [0],
                 }
             }
         }
@@ -54,6 +61,20 @@ output_opts = {
             'numPoints': 1,
             'ewsbBCscale': True ,
             }
+        }
+
+def setup_mu(r, mu):
+    r.useAlternativeEwsb()
+    r.setMuCond(mu)
+    r.setSusyMu(mu)
+
+def setup_ma(r, mA):
+    r.useAlternativeEwsb()
+    r.setMaCond(mA)
+
+setup_functions = { # function( MssmSoftSusy, Value )
+        'mA': setup_ma,
+        'mu': setup_mu,
         }
 
 #SPSLHAlib = cdll.LoadLibrary('packages/lib/libmcsoftsusy_slha.so')
@@ -116,9 +137,9 @@ class MssmSoftsusy(object) :
         if sz >= SLHA_MAX_SIZE:
             print "*** WARNING: string access has been truncated in softsusy"
         return c_str_buf.value
-
-def softsusy_set_MIXING(mixing):
-    SPlib.softsusy_set_MIXING(mixing)
+    def setMixing(self, mixing):
+        # this actually sets a global, but we dont like globals
+        SPlib.set_global_MIXING(mixing)
 
 class QedQcd(object) :
     def __init__(self) :
@@ -165,15 +186,12 @@ def run(model, **model_inputs):
     for var_name, value in models[model]['other_vars'].iteritems():
         low_args[var_name] =  model_inputs.get(var_name, value)
 
-    softsusy_set_MIXING(0)
+    for var in model_inputs:
+        if var in setup_functions:
+            setup_functions[var](r, model_inputs[var])
 
-    r.useAlternativeEwsb()
-    
-#    r.setSetTbAtMX(True)
-    print model_inputs['mu']
-    r.setMuCond(model_inputs['mu'])
-    r.setSusyMu(model_inputs['mu'])
-    r.setMaCond(model_inputs['mA'])
+    for func_name, args in models[model].get('other_setup',{}).iteritems():
+        getattr(r, func_name)(*args)
 
     r.lowOrg(model=model, dv_pars=inputs, qq_oneset=oneset, **low_args)
 
