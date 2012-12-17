dd7da023a171099306f8ed5613687d7a4ac30aa3
Sam Rogerson <samr@cern.ch>
2012-12-17 16:02:25 +0000

    clean up

--- mcrw/point.py
+++ mcrw/point.py
@@ -9,7 +9,6 @@ from interfaces import softsusy
 from interfaces import feynhiggs, micromegas, superiso, bphysics, lspscat
 from interfaces import susypope
 
-
 import utils
 
 slha_generator = softsusy
@@ -22,25 +21,21 @@ DEBUG = False
 def run_point(model, **inputs):
     stdouts = OrderedDict()
 
-    #print("Generating SLHA file...",)
     obj, stdout = utils.get_ctypes_streams(func=slha_generator.run,
             args=[model], kwargs=inputs)
     stdouts.update({slhamodule.name: stdout})
     slhafile = SLHA(obj)
     if DEBUG: print(obj)
-    #print("Done")
-    #with open('slhas/jad.slha','w') as f:
-       #f.write(obj)
 
     predictor_output = OrderedDict()
     for predictor in predictors:
-        #print("Running {n}...".format(n=predictor.name),)
         is_modifier = predictor in slha_modifiers
         result, stdout = utils.get_ctypes_streams(
                 func=slhamodule.send_to_predictor,
                 args=[slhafile,predictor, is_modifier])
         predictor_output.update(result)
         stdouts.update({predictor.name: stdout})
+
     if DEBUG:
         for name, stdout in stdouts.iteritems():
             print("="*80)
@@ -48,25 +43,17 @@ def run_point(model, **inputs):
             print("-"*80)
             print(stdout)
             print("="*80)
-        #print "Done"
+        for predictor, obs in predictor_output.iteritems():
+            print('')
+            print(utils.ansi_bold(predictor))
+            print("="*len(predictor))
+            x = max([len(n) for n,_ in obs.iteritems()])
+            f_str = "{{n:{x}}} = {{p}}".format(x=x)
+            for name,value in obs.iteritems():
+                if type(value) is not list:
+                    print(f_str.format(n=name, p=value))
+                else:
+                    print(f_str.format(n=name, p="[{0}, ... , {1}][{2}]".format(
+                        value[0],value[-1],len(value))))
 
-    for predictor, obs in predictor_output.iteritems():
-        print('')
-        print(utils.ansi_bold(predictor))
-        print("="*len(predictor))
-        x = max([len(n) for n,_ in obs.iteritems()])
-        f_str = "{{n:{x}}} = {{p}}".format(x=x)
-        for name,value in obs.iteritems():
-            if type(value) is not list:
-                print(f_str.format(n=name, p=value))
-            else:
-                print(f_str.format(n=name, p="[{0}, ... , {1}][{2}]".format(
-                    value[0],value[-1],len(value))))
-
-    #print slhafile
-
-    #with open('slhas/testPoint_output_fh.slha','w') as f:
-    #   f.write(slhafile)
-    #utils.pickle_object(slhafile, 'slhas/testPoint_output.pickled')
-    #unpickled = utils.open_pickled_file('slhas/testPoint_output.pickled')
-    #print unpickled
+    return slhafile.process(), predictor_output
