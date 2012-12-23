4b8b1c89b4ab23dba5a498c61bfd425fbe429237
Sam Rogerson <samr@cern.ch>
2012-12-23 11:07:21 +0000

    breakdown has (value, chi2) tuple

--- PointAnalyser/Analyse.py
+++ PointAnalyser/Analyse.py
@@ -19,6 +19,6 @@ def chi2(observations):
                 constraint = _constraints[name]
                 chi2 = constraint['func'](value, constraint['value'],
                         math.sqrt(sum([x**2 for x in constraint['error']])))
-                chi2_breakdown[predictor][name] = chi2
+                chi2_breakdown[predictor][name] = (value, chi2)
                 chi2_total += chi2
     return chi2_total, chi2_breakdown
