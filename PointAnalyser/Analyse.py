30a31e4b0b47054e903f7167173a418a723c5156
Sam Rogerson <samr@cern.ch>
2012-12-23 10:52:00 +0000

    analyse takes dict of all predictions

--- PointAnalyser/Analyse.py
+++ PointAnalyser/Analyse.py
@@ -2,16 +2,20 @@ import math
 
 from PointAnalyser import Constraints
 
-constraints = Constraints.base_constraints
+_constraints = Constraints.base_constraints
 
 def set_constraints(d):
-    global constraints
-    constraints = d.copy()
+    global _constraints
+    _constraints = d.copy()
 
 def chi2(predictions, value):
-    chi2 = 0.
-    if prediction in constraints:
-        constraint = constraints[prediction]
-        chi2 = constraint['func'](value, constraint['value'],
-                math.sqrt(sum([x**2 for x in constraint['error']])))
-    return chi2
+    chi2_total = 0.
+    chi2_breakdown = {}
+    for prediction in predictions:
+        if prediction in _constraints:
+            constraint = _constraints[prediction]
+            chi2 = constraint['func'](value, constraint['value'],
+                    math.sqrt(sum([x**2 for x in constraint['error']])))
+            chi2_breakdown[prediction] = chi2
+            chi2_total += chi2
+    return chi2_total, chi2_breakdown
