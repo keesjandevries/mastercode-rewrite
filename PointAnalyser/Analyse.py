b046fe4152a986f00db87212656393f6147827f7
Keesjan de Vries <keesjan777@gmail.com>
2013-01-11 19:24:50 +0000

    for the chi2 calculation loop over constraint names

--- PointAnalyser/Analyse.py
+++ PointAnalyser/Analyse.py
@@ -4,21 +4,17 @@ from collections import defaultdict
 
 from PointAnalyser import Constraints
 
-_constraints = Constraints.constraints
+_constraints_dict = Constraints.get_constraints_dict()
 
-def set_constraints(d):
-    global _constraints
-    _constraints = d.copy()
-
-def chi2(observations):
+def chi2(predictions,data_set=None):
+    #define here for now:
+    if data_set is None:
+        data_set=['Higgs125']
     chi2_total = 0.
     chi2_breakdown = defaultdict(dict)
-    for predictor, predictions in observations.items():
-        for name, value in predictions.items():
-            if name in _constraints:
-                constraint = _constraints[name]
-                chi2 = constraint['func'](value, constraint['value'],
-                        math.sqrt(sum([x**2 for x in constraint['error']])))
-                chi2_breakdown[predictor][name] = (value, chi2)
-                chi2_total += chi2
+    for name in data_set:
+        constraint=_constraints_dict[name]
+        chi2=constraint.get_chi2(predictions)
+        chi2_total+=chi2
+        chi2_breakdown[name] =  chi2
     return chi2_total, chi2_breakdown
