228959f28f0e6849fbd1cbc0879db6a99ae6af4f
Keesjan de Vries <keesjan777@gmail.com>
2013-01-11 19:23:13 +0000

    modify functions to take inputs meas and pred

--- PointAnalyser/LikelihoodFunctions.py
+++ PointAnalyser/LikelihoodFunctions.py
@@ -1,20 +1,34 @@
-def gauss(x, mu, sigma):
-    return ((x-mu)/sigma)**2
+import math
+def gauss(meas,pred):
+    x, mu, sigma=intern_get_x_mu_sigma(meas,pred)
+    return intern_gauss(x,mu,sigma) 
 
-def upperlimit(x, mu, sigma):
+def upperlimit(meas,pred):
+    x, mu, sigma=intern_get_x_mu_sigma(meas,pred)
     chi2 = 0
     if x > mu:
-        chi2 = gauss(x,mu,sigma)
+        chi2 = inter_gauss(x,mu,sigma)
     else:
         chi2 = 0
     return chi2
 
-def lowerlimit(x, mu, sigma):
+def lowerlimit(meas,pred):
+    x, mu, sigma=intern_get_x_mu_sigma(meas,pred)
     chi2 = 0
     if x < mu:
-        chi2 = gauss(x,mu,sigma)
+        chi2 = inter_gauss(x,mu,sigma)
     else:
         chi2 = 0
     return chi2
 
-#def chi2_from_cl(cl):
+def intern_get_x_mu_sigma(meas,pred):
+    assert len(pred) is 1
+    assert len(meas) > 1
+    x=pred[0]
+    mu=meas[0]
+    sigma= math.sqrt(sum([x**2 for x in meas[1:] ]))
+    assert sigma is not 0.
+    return x, mu , sigma 
+
+def intern_gauss(x,mu,sigma):
+    return ((x-mu)/sigma)**2
