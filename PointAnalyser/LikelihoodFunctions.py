8f6d5e5d0207702eb999f27eae4c1d193820ef23
Keesjan de Vries <keesjan777@gmail.com>
2013-01-15 15:30:35 +0000

    lhoodfunctions

--- PointAnalyser/LikelihoodFunctions.py
+++ PointAnalyser/LikelihoodFunctions.py
@@ -26,8 +26,11 @@ def power_n_scaling(bc, r,n):
     return bc*(r**n)
 
 def power_4_inv_single_contour(point, contour):
+    chi2=0.
     r = contour.point_ratio(point)
-    return power_n_inv_scaling(contour.chi2, r,4)
+    if r is not None:
+        chi2=power_n_inv_scaling(contour.chi2, r,4)
+    return chi2
 
 def power_2_single_contour(point,contour):
     r = contour.point_ratio(point)
@@ -41,5 +44,25 @@ def power_2_single_ma_tanb(point,contour):
         chi2=power_n_scaling(contour.chi2,r,2)
     return chi2
 
-#def xenon100_jul_2012(point,contour)
-    
+def xenon100_jul_2012(point,contour):
+    chi2=0.
+    r = contour.point_ratio(point)
+    if r is not None:
+        N = r*5.1   # the number of events on the line times the x-section ratio
+        mu = 1.     # these numbers were calculated by Jad
+        sigma = 2.7 #
+        chi2=gauss([N],mu,sigma)
+    else:
+        print("WARNING: no valid point ratio. Chi2 is set to 0.")
+    return chi2 
+
+def one_dim_chi2_lookup(point,contour):
+    chi2=0.
+    par=point[0]
+    x2=contour.get_contour_value(par)
+    if x2 is not None:
+        chi2=x2
+    else:
+        print("WARNING: problem with parameter lookup. Chi2 is set to 0.")
+    return chi2
+
