72495a9ab0d4c176c97d3d0327f4e84f5d3b4013
Keesjan de Vries <keesjan777@gmail.com>
2013-01-24 18:33:30 +0000

    implement manual setting of slha-file in point.py and here only

--- ObsCalculator/point.py
+++ ObsCalculator/point.py
@@ -19,12 +19,31 @@ predictors = slha_modifiers + [micromegas, superiso, bphysics, lspscat,#]
 def run_point(model, **input_pars):
     stdouts = OrderedDict()
 
+    # spectrum generation
     obj, stdout = tools.get_ctypes_streams(func=slha_generator.run,
             args=[model], kwargs=input_pars[slha_generator.name])
 
     stdouts.update({slhamodule.name: stdout})
     slhafile = SLHA(obj)
 
+    # "Manually" setting MW and MZ in the slha file object
+    # if 'mc_slha_update' is in input_vars
+    # this hack should not escape from this file
+    if input_pars.get('mc_slha_update'):
+        # by default, use these values
+        values={('MASS','MW') : 80.4,('SMINPUTS','MZ') : 91.1875}
+        # someone may define 'mc_slha_updata : True'
+        try:
+            values.update([(oid,val ) for oid,val in input_pars['mc_slha_update'].items() if oid in values.keys()])
+        except TypeError:
+            pass
+        for oid, val in values.items():
+            slhafile[oid]=val
+
+
+
+
+    # send slha file to predictors
     predictor_output = OrderedDict()
     for predictor in predictors:
         is_modifier = predictor in slha_modifiers
