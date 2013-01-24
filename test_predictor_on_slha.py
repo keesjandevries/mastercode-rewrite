df8d92417223e68e2ef157645e45604f24c2f06d
Keesjan de Vries <keesjan777@gmail.com>
2013-01-24 16:06:59 +0000

    just another test routine

---
+++ test_predictor_on_slha.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+import argparse, pprint
+
+# slha represntation
+from ObsCalculator.interfaces.slhalib import SLHA
+
+# spectrum calculator
+from ObsCalculator.interfaces import softsusy
+from ObsCalculator.interfaces import feynhiggs, micromegas, superiso, bphysics, lspscat
+from ObsCalculator.interfaces import susypope
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--filename', '-f', dest='filename', action='store', type=str,
+            default='slhas/test.slha', help='slha file name')
+
+    return parser.parse_args()
+
+if __name__=="__main__" :
+    args=parse_args()
+    filename=args.filename
+    slhafile=SLHA()
+    slhafile.read(filename)
+
+    
+    bpp = pprint.PrettyPrinter(indent=4, depth=5)
+    bpp.pprint(lspscat.run(slhafile))
+    
