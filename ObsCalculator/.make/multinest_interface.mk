0384205752fd1920c5f38974e6d533e10a4e0056
Keesjan de Vries <keesjan777@gmail.com>
2013-01-25 10:32:09 +0000

    tweek make files multinest

--- ObsCalculator/.make/multinest_interface.mk
+++ ObsCalculator/.make/multinest_interface.mk
@@ -6,13 +6,14 @@ multinest_interface: $(interface_lib) $(interface_src)
 
 interface_obj=$(interface_src:.c=.o)
 
+# This used to be g++ maybe it will give errors
 $(interface_lib): $(interface_obj)
-	$(cc) -shared -Wl,-soname,libmcmultinest.so -o $(interface_lib) \
+	$(xx) -shared -Wl,-soname,libmcmultinest.so -o $(interface_lib) \
 		-Wl,-rpath,$(lib_dir) \
 		$(interface_obj) -L$(src_dir) -l$(lib_short) -lgfortran -llapack -lpthread
 
 $(interface_obj): $(interface_src)
-	gcc -c -I. -DMULTINEST_CALL=__nested_MOD_nestrun \
+	$(cc) -c -I. -DMULTINEST_CALL=__nested_MOD_nestrun \
 		-O3 -std=c99 -Wall -Wextra -fPIC  -o $(interface_obj) $(interface_src) \
 		-lgfortran 
 
