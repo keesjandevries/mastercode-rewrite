0cc6d5cfe90d40105eff139993d638774a90887d
Keesjan de Vries <keesjan777@gmail.com>
2013-02-10 16:58:47 +0000

    storage at least outputs something

--- Storage/sqlstorage.py
+++ Storage/sqlstorage.py
@@ -12,17 +12,24 @@ def store_point_to_table(chi2,obs,table,filename):
 
     #with automatically closes the connnection
     with conn:
-        #FIXME: this can be done more effictiently, initialising only once!
-        #WARINING: this storage is dependent on how the predictions are parsed
-        chi2_data_type     ="chi2 DOUBLE"
-        slha_data_type_list=" DOUBLE, ".join(["_".join([first,block,comment]) for (first,(block,comment)), val in obs.items() if first == 'slha'])
-        pred_data_type_list=" DOUBLE, ".join(["_".join([first,second]) for (first, second), val in obs.items() if not first == 'slha'  ] )
-        name_data_type_list=", ".join([chi2_data_type,slha_data_type_list,pred_data_type_list])
-        print(name_data_type_list)
-        cur.execute("CREATE TABLE IF NOT EXISTS %s(%s)",(table,name_data_type_list))
-        fill_q_marks="?, "*(len(obs)+1)
-        #FIXME: this is probably slow!!
-        cur.execute(("INSERT INTO %s VALUES(%s)", (table,fill_q_marks) ),tuple([chi2]+list(obs)))
+        try:
+            cur=conn.cursor()
+            #FIXME: this can be done more effictiently, initialising only once!
+            if obs:
+                # FIXME: stupid way of doing this: collumn names are now again vars0 ... varN
+                name_data_type_list=" DOUBLE, ".join(["vars"+str(i) for i in range(len(obs)+1) ])
+                command="CREATE TABLE IF NOT EXISTS {0}({1})".format(table,name_data_type_list)
+                cur.execute(command)
+
+                fill_q_marks=", ".join([ "?" for i in range(len(obs)+1)])
+                #FIXME: this is probably slow!!
+                command="INSERT INTO {0} VALUES({1})".format(table,fill_q_marks)
+                cur.execute( command,tuple([chi2]+list(obs.values())))
+        except sqlite3.Error as e:
+            if conn:
+                conn.rollback()
+            print("ERROR in sqlite: ",e.args[0])
+
 
 
  
