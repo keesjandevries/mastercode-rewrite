45cf1b10b83413f83e451838fdd971c2ff3d9ebc
Keesjan de Vries <keesjan777@gmail.com>
2013-02-10 14:02:32 +0000

    loop over slhafiles to test

--- test_predictor_on_slha.py
+++ test_predictor_on_slha.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
-import argparse, pprint
+import argparse, pprint, sys
 
+from tools import pickle_object
 # slha represntation
 from ObsCalculator.interfaces.slhalib import SLHA
 
@@ -13,32 +14,48 @@ from ObsCalculator.interfaces import susypope
 from PointAnalyser import Constraints_list
 from PointAnalyser import Analyse
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('--filename', '-f', dest='filename', action='store', type=str,
-            default='slhas/test.slha', help='slha file name')
+#def parse_args():
+#    parser = argparse.ArgumentParser(description='Process some integers.')
+#    parser.add_argument('--save', '-s', dest='save', action='store', type=bool,
+#            default=False, help='slha file name')
+#
+#    return parser.parse_args()
+#def ouput_obj_to_pickle(d,filename):
+#    file=open(filename,'wb')
+#    pickle.dump(d,file)
+#    print( "Object written to picled file",filename)
+#    file.close()
 
-    return parser.parse_args()
 
 if __name__=="__main__" :
-    args=parse_args()
-    filename=args.filename
-    slhafile=SLHA()
-    slhafile.read(filename)
+    filenames=sys.argv[1:]
+    l=[]
+    slha_lookup=None
+    for filename in filenames:
+        slhafile=SLHA(lookup=slha_lookup)
+        slhafile.read(filename)
+#        if not slha_lookup:
+#            slha_lookup=slhafile.get_lookup()
 
-    
-    bpp = pprint.PrettyPrinter(indent=4, depth=5)
-    predictions=susypope.run(slhafile)
-#    predictions=lspscat.run(slhafile)
-    bpp.pprint(predictions)
-    
-    if True:
-        all_constraints=Constraints_list.constraints
-    #    data_set=all_constraints.keys()
-        data_set=['Al(SLD)', 'sintheta_eff', 'Gamma_Z',  'Rl', 'Afb(b)',  'Afb(c)',   'DAlpha_had', 
-                'sigma_had^0', 'Al(P_tau)', 'Ac', 'Rb', 'Rc', 'Ab',  'Al_fb',  'MW-mc-old', ]
-    #    print(data_set)
-        constraints={name: all_constraints[name] for name in data_set}
-        #pass this constraints list to the chi2 function
-        total, breakdown = Analyse.chi2(predictions,constraints)
-        bpp.pprint(breakdown)
+        
+        bpp = pprint.PrettyPrinter(indent=4, depth=5)
+        kwargs={'verbose':False}
+        if filename == 'slhas/susy-pope-1000_nuhm2/015762-slha.out': kwargs={'verbose':True}
+        predictions=susypope.run(slhafile,inputs=kwargs)
+#        predictions=lspscat.run(slhafile)
+        if filename == 'slhas/susy-pope-1000_nuhm2/015762-slha.out':   bpp.pprint(predictions)
+        
+        if True:
+            all_constraints=Constraints_list.constraints
+        #    data_set=all_constraints.keys()
+            data_set=['Al(SLD)', 'sintheta_eff', 'Gamma_Z',  'Rl', 'Afb(b)',  'Afb(c)',    
+                    'sigma_had^0', 'Al(P_tau)', 'Ac', 'Rb', 'Rc', 'Ab',  'Al_fb',  'MW-mc-old', ]
+        #    print(data_set)
+            constraints={name: all_constraints[name] for name in data_set}
+            #pass this constraints list to the chi2 function
+            total, breakdown = Analyse.chi2(predictions,constraints)
+            print('Total is:',total)
+            if filename == 'slhas/susy-pope-1000_nuhm2/015762-slha.out': bpp.pprint(breakdown)
+            l.append((breakdown,filename))
+    pickle_object(l,'temp/mcpp-breakdowns.pkl')
+
