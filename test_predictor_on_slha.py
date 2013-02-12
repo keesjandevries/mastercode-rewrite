208687bd5a6a43de6ee280c1737fd9102ddb09f7
Keesjan de Vries <keesjan777@gmail.com>
2013-02-12 10:28:23 +0000

    also change to sensible naming

--- test_predictor_on_slha.py
+++ test_predictor_on_slha.py
@@ -49,7 +49,7 @@ if __name__=="__main__" :
             all_constraints=Constraints_list.constraints
         #    data_set=all_constraints.keys()
             data_set=['Al(SLD)', 'sintheta_eff', 'Gamma_Z',  'Rl', 'Afb(b)',  'Afb(c)',    
-                    'sigma_had^0', 'Al(P_tau)', 'Ac', 'Rb', 'Rc', 'Ab',  'Al_fb',  'MW-mc-old', ]
+                    'sigma_had^0', 'Al(P_tau)', 'Ac', 'Rb', 'Rc', 'Ab',  'Afb_l',  'MW-mc-old', ]
         #    print(data_set)
             constraints={name: all_constraints[name] for name in data_set}
             #pass this constraints list to the chi2 function
