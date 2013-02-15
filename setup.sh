2d15f9f0e381cc49223e9785b9ff2e87115a3a7e
Keesjan de Vries <keesjan777@gmail.com>
2013-02-15 12:41:10 +0000

    new setup using my own gcc

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-export PATH=/home/hep/sr505/local/bin:${PATH}
-export LD_LIBRARY_PATH=/home/hep/sr505/local/lib:/home/hep/sr505/local/lib64:${LD_LIBRARY_PATH}
+#export PATH=/home/hep/kjd110/local/bin:${PATH}
+#export LD_LIBRARY_PATH=/home/hep/kjd110/local/lib:/home/hep/kjd110/local/lib64:${LD_LIBRARY_PATH}
+export PATH=/home/hep/kjd110/local/bin:/home/hep/sr505/local/bin:${PATH}
+export LD_LIBRARY_PATH=/home/hep/kjd110/local/lib:/home/hep/kjd110/local/lib64:/home/hep/sr505/local/lib:/home/hep/sr505/local/lib64:${LD_LIBRARY_PATH}
