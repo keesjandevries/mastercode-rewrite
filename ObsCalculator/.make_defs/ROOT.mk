8cc1f8ea181a05d411d902be9672e8609cfaecfe
Keesjan de Vries <keesjan777@gmail.com>
2013-02-15 17:56:27 +0000

    update definitions for interface

--- ObsCalculator/.make_defs/ROOT.mk
+++ ObsCalculator/.make_defs/ROOT.mk
@@ -3,12 +3,12 @@ name=root_v$(version)
 src_dir=$(UTIL_DIR)/root
 lib_dir=$(LIB_DIR)
 lib=$(INSTALL_DIR)/lib/libPyROOT.so
-lib_short=SLHA
 tar_name=$(name).source.tar.gz
 remote=ftp://root.cern.ch/root/$(tar_name)
 tarfile=$(TAR_DIR)/$(tar_name)
-#interface_src=$(INTERFACE_DIR)/slhalib.cc
-#interface_lib=$(LIB_DIR)/libmcslhalib.so
+interface_src=$(STORAGE_DIR)/interfaces/ROOT.cc
+interface_lib=$(LIB_DIR)/libmcROOT.so
+root_flags=$(shell $(INSTALL_DIR)/bin/root-config --cflags --libs )
 
-#cc=g++
+cc=g++
 #fc=gfortran
