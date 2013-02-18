ce644a92f7f4dd1aef2e91970715043c97977ef8
Keesjan de Vries <keesjan777@gmail.com>
2013-02-18 22:57:04 +0000

    state of the art cmssm predictor

--- mc_cmssm_point.py
+++ mc_cmssm_point.py
@@ -18,6 +18,7 @@ def parse_args():
     parser.add_argument('--observables', '-o', dest='obs'      , action='store_true', help='print observables')
     parser.add_argument('--breakdown'  , '-b', dest='breakdown', action='store_true', help='print X^2 breakdonw')
     parser.add_argument('--root_save'  , '-r', dest='root_save', action='store_true', help='save to root file')
+    parser.add_argument('--verbose'    , '-v', dest='verbose'  , action='store', nargs="+", help='verbosity')
     parser.add_argument('--input_pars', '-p', dest='input_pars', action='store', type=str,
             default=None, help='override all_params')
     return parser.parse_args()
@@ -29,13 +30,12 @@ if __name__=="__main__" :
 
     all_params={
             'SoftSUSY':{
-                'm0'    : 300.53, 
-                'm12'   : 905.0, 
-                'A0'    : -1303.97 , 
-                'tanb'  : 16.26, 
-                'sgnMu' : 1,
-                'mt'    : 173.2,
-                'mgut'  : 2e16,     # FIXME: this should become a default
+                ('MINPAR', 'M0'):       300.53,
+                ('MINPAR', 'M12'):      905.0,
+                ('MINPAR', 'TB'):       16.26,
+                ('MINPAR', 'signMUE'):  1.0,
+                ('MINPAR', 'A'):        -1303.97,
+
                 },
             'mc_slha_update':{
                 ('SMINPUTS','MZ')   : 91.1876, 
@@ -46,14 +46,20 @@ if __name__=="__main__" :
                     }
                 }
             }
+    #check for command line input parameters
     if args.input_pars:
         all_params.update(eval(args.input_pars))
-
+        
+    #check verbosity
+    if args.verbose:
+        all_params['verbose']=args.verbose
     try:
         slha_obj, observations,stdouts = point.run_point(model=model, **all_params)
     except TypeError:
         print("ERROR: Point failed to run")
         exit()
+
+
 
     slha_file=slha_obj.process()
     combined_obs = OrderedDict(list(slha_file.items()) + list(observations.items()))
