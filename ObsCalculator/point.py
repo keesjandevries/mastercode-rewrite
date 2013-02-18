005ea7ddd7fb77b75539c1c4f580cf95adcb6e6f
Keesjan de Vries <keesjan777@gmail.com>
2013-02-18 22:56:49 +0000

    now with great error handling

--- ObsCalculator/point.py
+++ ObsCalculator/point.py
@@ -7,11 +7,11 @@ from ObsCalculator.interfaces import slhalib as slhamodule
 from ObsCalculator.interfaces.slhalib import SLHA
 
 # spectrum calculator
-from ObsCalculator.interfaces import softsusy
+from ObsCalculator.interfaces import softsusy2
 from ObsCalculator.interfaces import feynhiggs, micromegas, superiso, bphysics, lspscat
 from ObsCalculator.interfaces import susypope
 
-slha_generator = softsusy
+slha_generator = softsusy2
 slha_modifiers = [feynhiggs]
 predictors = slha_modifiers + [micromegas, superiso, bphysics, lspscat,#]
         susypope]
@@ -19,26 +19,42 @@ predictors = slha_modifiers + [micromegas, superiso, bphysics, lspscat,#]
 
 def run_point(model, **input_pars):
     stdouts = OrderedDict()
+    #handle verbosity like this 'verbose': ['softsusy', 'FH', ...]
+    #verbosity helps with debugging
+    if not input_pars.get('verbose'): input_pars['verbose']=[]
+    #make an option to select all verbosity
+    if 'all' in input_pars['verbose']:
+        input_pars['verbose']=[pred.name for pred in predictors]
+        input_pars['verbose'].append(slha_generator.name)
+        input_pars['verbose'].append('slha')
 
     # spectrum generation
+    # check for verbosity
+    slha_gen_verbose=slha_generator.name in input_pars['verbose']
+    # run the spectrum calculator
     (obj,err), stdout = tools.get_ctypes_streams(func=slha_generator.run,
-            args=[model], kwargs=input_pars[slha_generator.name])
-    # verbosity can be set as an input, useful for debugging
-    if input_pars.get('verbose'):
-        #FIXME: maybe specify verbose like 'verbose':['spectrum_calculator','feynhiggs',..] 
-        print("slhafile:")
-        print(obj)
-        print("stdout:")
+            args=[model,input_pars[slha_generator.name]], kwargs={'verbose':slha_gen_verbose})
+    # print standard out 
+    if slha_gen_verbose:
         print(stdout)
-        if err: print("ERROR: spectrum calculator")
+    #error handling for slha generator    
+    if err: print("ERROR: spectrum calculator")
     
     # If the spectrum calculater fails then there is no hope to calculate anything else.
     # Hence, return None is appropriate
     if(err): return None
 
     stdouts.update({slhamodule.name: stdout})
+
+    # if you want to see the slha file from the specturm generator, put 'verbose' in the 
+    if 'slha' in input_pars['verbose']:
+        print(obj)
+
+    # make slha object 
     slhafile = SLHA(obj,input_pars.get('lookup'))
 
+    # WARNING: here is a functionality needed by mastercode
+    #          it is not generic
     # "Manually" setting MW and MZ in the slha file object
     # if 'mc_slha_update' is in input_vars
     # this hack should not escape from this file
@@ -54,15 +70,18 @@ def run_point(model, **input_pars):
             slhafile[oid]=val
 
 
-    # send slha file to predictors
+    # Finally: send slha file to predictors
     predictor_output = OrderedDict()
     for predictor in predictors:
         is_modifier = predictor in slha_modifiers
-        #FIXME: the other predictors don't have error handling yet
+        #FIXME: needs to get something like: pred_verbose=  predictor.name in input_pars['verbose']
+        # and pred_verbose as one of tdhe options, 
+        pred_verbose=(predictor.name in input_pars['verbose'])
         result, stdout = tools.get_ctypes_streams(
                 func=slhamodule.send_to_predictor,
                 args=[slhafile,input_pars.get(predictor.name),predictor, is_modifier])
-        if input_pars.get('verbose'):  print(stdout)
+        #FIXME: if pred_verbose: print
+        if pred_verbose:  print(stdout)
         predictor_output.update(result)
         stdouts.update({predictor.name: stdout})
 
