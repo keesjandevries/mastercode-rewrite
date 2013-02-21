3a128f9e043adf0dcf6e05b0b4cd7c0093c7ad31
Keesjan de Vries <keesjan777@gmail.com>
2013-02-21 00:46:42 +0000

    add definitions for reading interface

--- ObsCalculator/.make_defs/ROOT.mk
+++ ObsCalculator/.make_defs/ROOT.mk
@@ -8,6 +8,8 @@ remote=ftp://root.cern.ch/root/$(tar_name)
 tarfile=$(TAR_DIR)/$(tar_name)
 interface_src=$(STORAGE_DIR)/interfaces/ROOT.cc
 interface_lib=$(LIB_DIR)/libmcROOT.so
+interface_read_src=$(STORAGE_DIR)/interfaces/ROOT_read.cc
+interface_read_lib=$(LIB_DIR)/libmcROOT_read.so
 root_flags=$(shell $(INSTALL_DIR)/bin/root-config --cflags --libs )
 
 cc=g++
