0b012cdb3dbff46fdbacd2b7dbbe8f123b01a44a
Keesjan de Vries <keesjan777@gmail.com>
2013-03-11 16:09:14 +0000

    constraints defs

---
+++ ObsCalculator/.make_defs/constraints.mk
@@ -0,0 +1,7 @@
+src_dir=$(POINTAN_DIR)/cpp-constraint-classes
+obj=$(src_dir)/Constraints.o
+
+interface_src=$(POINTAN_DIR)/interfaces/constraints.cc
+interface_lib=$(LIB_DIR)/libmconstraints.so
+
+cc=g++
