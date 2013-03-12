d4663ec70ea5bf6ca03b121f5dc4ac41ef7495a9
Keesjan de Vries <keesjan777@gmail.com>
2013-03-12 14:08:11 +0000

    added more dependencies so that changes in the source code will make the interface recompile

--- ObsCalculator/.make/constraints_interface.mk
+++ ObsCalculator/.make/constraints_interface.mk
@@ -2,11 +2,15 @@ include $(DEF_DIR)/constraints.mk
 
 .PHONY: clean all
 
+objs=$(srcs:%.cc=%.o)
+
 constraints_interface: $(interface_lib) $(interface_src)
 
 interface_obj=$(interface_src:.cc=.o)
 
-$(interface_lib): $(interface_obj)
+#FIXME: want to make sure that dependencies ensure that the interface lib is recompiled 
+# if the lib was recompiled. Not sure if this is completely functional, though it seems to be
+$(interface_lib): $(interface_obj)  $(lib)
 	$(cc) -shared -Wl,-soname,libmcconstraints.so -o $(interface_lib) \
 		-Wl,-rpath,$(LIB_DIR) \
 		$(interface_obj) -L$(LIB_DIR) -lconstraints
@@ -14,6 +18,12 @@ $(interface_lib): $(interface_obj)
 $(interface_obj): $(interface_src)
 	$(cc) -c -fPIC -o $(interface_obj) $(interface_src) \
 		-I$(INCLUDE_DIR)
+
+#FIXME: want to make sure that dependencies ensure that the interface lib is recompiled 
+# if any changes are made in the source files.
+# Not entirely sure if this is completely functional, though it seems to be
+$(lib) : $(srcs)  $(src_dir)/*h
+	$(MAKE) -f $(MAKE_DIR)/.make/constraints.mk   
 
 clean:
 	-rm -f $(interface_obj) $(interface_lib)
