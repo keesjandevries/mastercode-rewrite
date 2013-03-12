a68b33d88bc109bf1074f09efee17db25a69329b
Keesjan de Vries <keesjan777@gmail.com>
2013-03-12 19:57:39 +0000

    should be the right dependencies for chi2functions

--- ObsCalculator/.make/chi2functions.mk
+++ ObsCalculator/.make/chi2functions.mk
@@ -2,16 +2,18 @@ include $(DEF_DIR)/chi2functions.mk
 
 objs=$(srcs:%.cc=%.o)
 
-chi2functions: $(lib)  $(src_dir)/*h
+chi2functions: $(lib)  
 	cp $(src_dir)/*h $(INCLUDE_DIR)
 
+#FIXME: should this library be linked against the constraints library..
 $(lib)	: $(objs)
 	$(ld) $(ldflags)  $(lib) $^ 
 
 #FIXME: I don't understand why %.o makes sure the right directories are checked
 # This does seem to work though..
-%.o : %.cc 
-	$(cc) -c -fPIC  -o $@ $< -I$(src_dir) -I$(INCLUDE_DIR)
+#FIXME: really need to think whether this is the proper way of handling the dependencies
+%.o : %.cc %.h  $(constraints_dir)/*.h
+	$(cc) -c -fPIC -Wall  -o $@ $< -I$(src_dir) -I$(constraints_dir)
 
 .PHONY: clean all
 clean:
--- ObsCalculator/.make_defs/chi2functions.mk
+++ ObsCalculator/.make_defs/chi2functions.mk
@@ -6,6 +6,8 @@ lib=$(LIB_DIR)/lib$(name).a
 interface_src=$(POINTAN_DIR)/interfaces/$(name).cc
 interface_lib=$(LIB_DIR)/libmc$(name).so
 
+constraints_dir=$(POINTAN_DIR)/cpp-constraints-classes
+
 cc=g++
 ld=ar
 ldflags=rvs 
