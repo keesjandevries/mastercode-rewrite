0bb22d7d0063114fc5983df7dfb4c2bb105389c0
Keesjan de Vries <keesjan777@gmail.com>
2013-03-12 19:59:14 +0000

    dependencies seem to make sense now

--- ObsCalculator/.make/constraints_interface.mk
+++ ObsCalculator/.make/constraints_interface.mk
@@ -1,19 +1,16 @@
 include $(DEF_DIR)/constraints.mk
 
-.PHONY: clean all
 
 objs=$(srcs:%.cc=%.o)
-
-constraints_interface: $(interface_lib) $(interface_src)
-
 interface_obj=$(interface_src:.cc=.o)
 
+constraints_interface: $(interface_lib)  
 #FIXME: want to make sure that dependencies ensure that the interface lib is recompiled 
 # if the lib was recompiled. Not sure if this is completely functional, though it seems to be
-$(interface_lib): $(interface_obj)  $(lib)
-	$(cc) -shared -Wl,-soname,libmcconstraints.so -o $(interface_lib) \
+$(interface_lib): $(interface_obj)  $(lib) $(chi2functions_lib)
+	$(cc) -shared -Wall -Wl,-soname,libmcconstraints.so -o $(interface_lib) \
 		-Wl,-rpath,$(LIB_DIR) \
-		$(interface_obj) -L$(LIB_DIR) -lconstraints
+		$(interface_obj) -L$(LIB_DIR)  -lchi2functions -lconstraints
 
 $(interface_obj): $(interface_src)
 	$(cc) -c -fPIC -o $(interface_obj) $(interface_src) \
@@ -22,8 +19,15 @@ $(interface_obj): $(interface_src)
 #FIXME: want to make sure that dependencies ensure that the interface lib is recompiled 
 # if any changes are made in the source files.
 # Not entirely sure if this is completely functional, though it seems to be
+$(chi2functions_lib) : $(chi2functions_srcs) $(chi2functions_incs) $(lib)
+	$(MAKE) -f $(MAKE_DIR)/.make/chi2functions.mk
+
 $(lib) : $(srcs)  $(src_dir)/*h
 	$(MAKE) -f $(MAKE_DIR)/.make/constraints.mk   
 
+
+
+#FIXME: not sure if PHONY is needed here
+.PHONY: clean all
 clean:
 	-rm -f $(interface_obj) $(interface_lib)
--- ObsCalculator/.make_defs/constraints.mk
+++ ObsCalculator/.make_defs/constraints.mk
@@ -1,9 +1,15 @@
+name=constraints
 src_dir=$(POINTAN_DIR)/cpp-constraints-classes
 srcs=$(wildcard $(src_dir)/*cc)
-lib=$(LIB_DIR)/libconstraints.a
+lib=$(LIB_DIR)/lib$(name).a
 
-interface_src=$(POINTAN_DIR)/interfaces/constraints.cc
-interface_lib=$(LIB_DIR)/libmcconstraints.so
+interface_src=$(POINTAN_DIR)/interfaces/$(name).cc
+interface_lib=$(LIB_DIR)/libmc$(name).so
+
+chi2functions_lib=$(LIB_DIR)/libchi2functions.a
+chi2functions_scr_dif=$(USER_DIR)/chi2_functions
+chi2functions_srcs=$(wildcard $(chi2functions_scr_dif)/*c)
+chi2functions_incs=$(wildcard $(chi2functions_scr_dif)/*h)
 
 cc=g++
 ld=ar
