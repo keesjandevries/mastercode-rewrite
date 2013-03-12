5fadbf0d35b36106816ed1e71d57cbde4f2dd9df
Keesjan de Vries <keesjan777@gmail.com>
2013-03-12 18:15:30 +0000

    the constraints interfaces for (so far) gaussian constraints only

--- PointAnalyser/interfaces/constraints.cc
+++ PointAnalyser/interfaces/constraints.cc
@@ -1,11 +1,24 @@
 #include "Constraints.h"
+#include "Chi2Functions.h"
 
 extern "C"{
     GaussConstraint * new_GaussConstraint(int *int_obs_ids, int len_int_obs_ids, 
-            double * gauss_data, int len_gauss_data, GaussFunc chi2function ){
+            double * gauss_data, int len_gauss_data, char * chi2function_name ){
+        // initialise int observalbe ids 
         std::vector<int> int_obs_ids_vec(int_obs_ids,int_obs_ids + len_int_obs_ids);
+        // inisialise data vector
         std::vector<double> gauss_data_vec(gauss_data,gauss_data + len_gauss_data);
-        return new GaussConstraint(int_obs_ids_vec,gauss_data_vec,chi2function);
+        // get chi2 function
+        std::string str_chi2function_name(chi2function_name);
+        std::map<std::string,GaussFunc> GaussFunc_map = get_GaussFunc_map();
+        // check if str_chi2function_name is in GaussFunc_map
+        if (GaussFunc_map.find(str_chi2function_name)!=GaussFunc_map.end()){
+            GaussFunc chi2function=GaussFunc_map[str_chi2function_name];
+            return new GaussConstraint(int_obs_ids_vec,gauss_data_vec,chi2function);
+        }
+        else{
+            return NULL;
+        }
     }
 
     double get_GaussChi2(GaussConstraint* gauss_constraint, double* obs ){
--- PointAnalyser/interfaces/constraints.py
+++ PointAnalyser/interfaces/constraints.py
@@ -1,16 +1,30 @@
 #! /usr/bin/env python
 
-from ctypes import cdll, c_void_p , c_int
+from ctypes import cdll, c_void_p , c_int, c_double
 from tools import ctypes_field_values, return_c_double_array, return_c_int_array
 
 ConstraintsLib = cdll.LoadLibrary('packages/lib/libmcconstraints.so')
 
-#placeholder
-def my_func(a,b):
-    return a-b
+
 
 class Constraint(object):
-    def __init__(self, oids, data, func):
+    """
+This Constraint class uses the constraint interface, which wraps
+the c++ classes defined in Constraints.h.
+
+Both GaussConstraint's and ContourConstraint's containt a memberfunction
+GetChi2(double *). The argument to this function is a double array, and the 
+position of the relevant observables is indicated by integers _internal_oids.
+
+In the implementation here, these _internal_oids will be [0] , [0,1], ... depending
+on how many observables are relevant for the calculation of the constraint.
+
+The double array with these observables is obtained like this obs=[point[oid] for oid in self._oids]
+where point is a dictionary, which can be accessed by self._oids.
+
+This explanation should suffice to understand the code below.
+    """
+    def __init__(self, oids, data, func_name):
         self._oids=oids
         int_oids=[i for i in range(len(oids))]
         c_ints=return_c_int_array(int_oids)
@@ -18,10 +32,12 @@ class Constraint(object):
         #FIXME: not sure if c_ints are needed here
         c_ints_len=c_int(len(c_ints))
         c_data_len=c_int(len(c_data))
-        self._obj=ConstraintsLib.new_GaussConstraint(c_ints,c_ints_len,c_data,c_data_len,c_void_p(func))
+        self._obj=ConstraintsLib.new_GaussConstraint(c_ints,c_ints_len,c_data,c_data_len,func_name.encode('ascii'))
     
     def get_chi2(self,point):
         obs=[point[oid] for oid in self._oids]
         c_obs=return_c_double_array(obs)
+        # very important: set the return type to double
+        ConstraintsLib.get_GaussChi2.restype=c_double
         return ConstraintsLib.get_GaussChi2(c_void_p(self._obj), c_obs)
 
