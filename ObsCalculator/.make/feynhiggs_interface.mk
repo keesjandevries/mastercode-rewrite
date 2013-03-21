adba9fb0794603ac57adaa56ac2788978c0888b7
Keesjan de Vries <keesjan777@gmail.com>
2013-03-21 19:55:44 +0000

    zal wel kloppen

--- ObsCalculator/.make/feynhiggs_interface.mk
+++ ObsCalculator/.make/feynhiggs_interface.mk
@@ -7,12 +7,12 @@ feynhiggs_interface: $(interface_lib) $(interface_src)
 interface_obj=$(interface_src:.cc=.o)
 
 $(interface_lib): $(interface_obj)
-	$(cc) -shared -Wl,-soname,libmcfeynhiggs.so -o $(interface_lib) \
+	$(cc) -g -shared -Wl,-soname,libmcfeynhiggs.so -o $(interface_lib) \
 		-Wl,-rpath,$(LIB_DIR) \
-		$(interface_obj) -L$(lib_dir) -l$(lib_short) -lgfortran
+		$(interface_obj) -L$(lib_dir) -l$(lib_short) -lgfortran -lm
 
 $(interface_obj): $(interface_src)
-	$(cc) -c -fPIC -o $(interface_obj) $(interface_src) \
+	$(cc) -g -c -fPIC -o $(interface_obj) $(interface_src) \
 		-I$(INCLUDE_DIR)
 
 clean:
