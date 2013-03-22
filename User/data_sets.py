503a04d398a136074d4d746350cd064a26d62fb3
Keesjan de Vries <keesjan777@gmail.com>
2013-03-22 11:03:10 +0000

    want to have data sets defined in separate files, first step to configuration

---
+++ User/data_sets.py
@@ -0,0 +1,6 @@
+#WARNING: RESULT ORIENTED, this module needs more thought at some point
+mc8= [ 'Al(SLD)', 'Ab', 'Ac', 'Oh^2_mc8', 'Higgs125', 'BR(Bd->ll)',  
+            'Gamma_Z', 'GZ_in', 'R(B->Xsll)', 'Al(P_tau)', 'MZ', 'R(D_ms)', 'MW', 'Afb_l', 
+            'xenon100', 'DAlpha_had', 'R(Delta_mk)',  'sigma_had^0', 'Afb(c)', 
+            'atlas5_m0_m12', 'Afb(b)',  'R(b->sg)', 'R(Dms)/R(Dmd)', 'R(B->taunu)', 
+            'Rc', 'Rb',  'Rl', 'mc8_bsmm', 'sintheta_eff', 'Mt', 'R(K->lnu)', 'R(Kp->pinn)', 'gminus2mu', 'MATANB' ]
