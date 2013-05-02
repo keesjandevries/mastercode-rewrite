65096592155237062c85fbf2e758f0722bd9427c
Keesjan de Vries <keesjan777@gmail.com>
2013-05-02 12:57:42 +0100

    succeeded to implement X2 calculation

--- test_sql_to_chi2_root.py
+++ test_sql_to_chi2_root.py
@@ -7,30 +7,49 @@ import Storage.sqlite_storage as sql
 # root storage modules
 import Storage.interfaces.ROOT as root
 from Storage import old_mc_rootstorage 
+#
+
+#X^2 calculation data sets
+from User.data_sets import data_sets
+from PointAnalyser import Analyse
+from PointAnalyser import Constraints_list
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Fill points randomly for the moment')
     parser.add_argument('--input-file','-i', dest='input_file', action='store', 
               help='Name of input date base file')
-    parser.add_argument('--n-points','-n', dest='n_points', action='store', type=int, default=10000,
+    parser.add_argument('--n-points','-n',  action='store', type=int, default=10000,
               help='Name of input date base file')
-    parser.add_argument('--root_save'  , '-r', dest='root_save', action='store_true', help='save to root file')
+    parser.add_argument('--data-set'  ,  default="mc8", help='data set for X^2 calculation')
+    parser.add_argument('--root-save'  , '-r',   help='save to root file')
     return parser.parse_args()
-
 
 if __name__=="__main__" :
     con = None
     args= parse_args()
+    #initialise constraints once
+    constraints={name: Constraints_list.constraints[name] for name in data_sets[args.data_set]}
     try:
-        #connection and cursor
+        #connection 
         con=sqlite3.connect(args.input_file)
+        #use row factory (see sqlite3 python documentation sqlite3.Row)
+        con.row_factory=sqlite3.Row
+        #cursor
         cur=con.cursor()
-        #retrieve a point
+        #number of points
         n_points=args.n_points
-        root.root_open('temp/test.root')
+        #open root file
+        root.root_open(args.root_save)
+        #get observables lookuk
+        lookup = sql.get_observable_ids(con,cur)    # lookup={col1: ( .. , .. ) , .... }
+        #FIXME: this statement should make a sensible selection, for now just a number of points
         cur.execute('select * from points limit {}'.format(n_points))
         for row in cur:
-            root.root_write(row)
+            point={ oid: row[col]  for col, oid in lookup.items()}
+            total, breakdown = Analyse.chi2(point,constraints)
+            point[('tot_X2', 'all')]=total
+            old_mc_rootstorage.write_point_to_root(point)
         root.root_close()
     
     # Finalise ...
@@ -42,13 +61,3 @@ if __name__=="__main__" :
     finally:
         if con:
             con.close()
-#
-#    # save to root
-#    if args.root_save:
-#        # NOTE: for old_mc_rootstorage, need X^2 
-#        root.root_open('temp/test.root')
-#        total=5.0
-#        for combined_obs in points:
-#            combined_obs[('tot_X2','all')]=total
-#            old_mc_rootstorage.write_point_to_root(combined_obs)
-#        root.root_close()
