e7da8e212a930e7152b40ca6fbc7f2b68fcf782e
Keesjan de Vries <keesjan777@gmail.com>
2013-05-13 18:37:47 +0100

    add print_chi2_breakdown

--- PointAnalyser/Analyse.py
+++ PointAnalyser/Analyse.py
@@ -14,3 +14,20 @@ and constraints, which are literaly the classes.
         chi2_breakdown[name]=chi2
         chi2_total+=chi2
     return chi2_total, chi2_breakdown
+
+def print_chi2_breakdown(point,constraints):
+    header='{:<20}: {:>13} | {:<50} '.format('Name constraint','Chi2 penalty','Observables')
+    print('='*len(header))
+    print(header)
+    print('='*len(header))
+    total=0.
+    for name, constraint in constraints.items(): 
+        oids= constraint.get_oids()
+        chi2= constraint.get_chi2(point)
+        print('{:<20}: {:>13.3f} | {} '.format(name,chi2, ['{:<30} : {:.6}'.format(str(oid),point[oid]) for oid in oids[:2] ] ))
+        total+=chi2
+    print('='*len(header))
+    print('Total Chi2: {}'.format(total))
+
+
+
