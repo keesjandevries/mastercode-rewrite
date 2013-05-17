3c0497524a3775dcb642d34a07ac5de6cce0ed27
Keesjan de Vries <keesjan777@gmail.com>
2013-05-17 16:01:58 +0100

    add parameter ranges

--- mc_pmssm8_ranges.py
+++ mc_pmssm8_ranges.py
@@ -4,12 +4,22 @@
 import json
 #For the degenerate masses want to scan Highest and lowers from CMSSM bf
 ranges={
-        'msq12':   [1725. ,1810. ],
-        'msq3':    [1300. , 1600. ],
-        'msl':     [390. ,660. ],
-        'M1':      [330. ,400. ],
-        'A':       [-3300.,-1600  ],
+        'msq12':   [0.      ,5000. ],
+        'msq3':    [0.      ,5000. ],
+        'msl':     [0.      ,5000. ],
+        'M1':      [0.      ,5000. ],
+        'A':       [-5000.  ,5000. ],
+        'MA':      [0.      ,5000. ],
+        'tanb':    [1.      ,60.   ],
+        'mu':      [-5000.  ,5000. ],
         }
 
-with open('User/pmssm_ranges.json','w') as f:
+with open('User/pmssm8_ranges.json','w') as f:
     json.dump(ranges, f)
+
+#These were the parameters used for an initial scan.
+#        'msq12':   [1725. ,1810. ],
+#        'msq3':    [1300. , 1600. ],
+#        'msl':     [390. ,660. ],
+#        'M1':      [330. ,400. ],
+#        'A':       [-3300.,-1600  ],
