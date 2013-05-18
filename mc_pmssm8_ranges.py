ae75649be601ae90cff4b2aa2b23f1b7337f7df0
Keesjan de Vries <keesjan777@gmail.com>
2013-05-18 16:15:06 +0100

    facilitate testing and real parameter setting

--- mc_pmssm8_ranges.py
+++ mc_pmssm8_ranges.py
@@ -3,7 +3,8 @@
 #very simply dumps pmssm8 ranges into a json file
 import json
 #For the degenerate masses want to scan Highest and lowers from CMSSM bf
-ranges={
+ranges={'full':
+{
         'msq12':   [0.      ,5000. ],
         'msq3':    [0.      ,5000. ],
         'msl':     [0.      ,5000. ],
@@ -12,10 +13,21 @@ ranges={
         'MA':      [0.      ,5000. ],
         'tanb':    [1.      ,60.   ],
         'mu':      [-5000.  ,5000. ],
-        }
+        },
+'testing':{
+        'msq12':   [1725. ,1810. ],
+        'msq3':    [1300. , 1600. ],
+        'msl':     [390. ,660. ],
+        'M1':      [330. ,400. ],
+        'A':       [-3300.,-1600  ],
+        'MA':      [1410.      ,1420. ],
+        'tanb':    [14.      ,15.   ],
+        'mu':      [660.  ,680. ],
+    },
+}
 
 with open('User/pmssm8_ranges.json','w') as f:
-    json.dump(ranges, f)
+    json.dump(ranges['testing'], f)
 
 #These were the parameters used for an initial scan.
 #        'msq12':   [1725. ,1810. ],
