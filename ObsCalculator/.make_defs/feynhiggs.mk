d4da740ab48e165d90da1353514d596859c122f3
Keesjan de Vries <keesjan777@gmail.com>
2013-05-29 15:41:37 +0100

    changes to definitions take care of shift in FeynHiggs version

--- ObsCalculator/.make_defs/feynhiggs.mk
+++ ObsCalculator/.make_defs/feynhiggs.mk
@@ -1,4 +1,5 @@
-version=2.8.7
+#version=2.8.7
+version=2.9.5
 name=FeynHiggs-$(version)
 src_dir=$(PREDICTOR_DIR)/$(name)
 machine=$(shell uname -m)
@@ -10,11 +11,12 @@ endif
 lib_dir=$(INSTALL_DIR)/$(machine_lib_dir)
 lib=$(lib_dir)/libFH.a
 lib_short=FH
-tar_name=$(name).tar
+#tar_name=$(name).tar
+tar_name=$(name).tar.gz
 remote_url=http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/newversion/
 remote=$(remote_url)/$(tar_name)
 tarfile=$(TAR_DIR)/$(tar_name)
-interface_src=$(INTERFACE_DIR)/feynhiggs.cc
+interface_src=$(INTERFACE_DIR)/feynhiggs$(version).cc
 interface_lib=$(LIB_DIR)/libmcfeynhiggs.so
 
 cc=g++
