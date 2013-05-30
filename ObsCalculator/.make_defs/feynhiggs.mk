147d3703f8e05126c0cfcf5b3a4025872bd82aef
Keesjan de Vries <keesjan777@gmail.com>
2013-05-30 15:57:08 +0100

    changin back to fh287 for the moment

--- ObsCalculator/.make_defs/feynhiggs.mk
+++ ObsCalculator/.make_defs/feynhiggs.mk
@@ -1,5 +1,5 @@
-#version=2.8.7
-version=2.9.5
+version=2.8.7
+#version=2.9.5
 name=FeynHiggs-$(version)
 src_dir=$(PREDICTOR_DIR)/$(name)
 machine=$(shell uname -m)
@@ -11,8 +11,8 @@ endif
 lib_dir=$(INSTALL_DIR)/$(machine_lib_dir)
 lib=$(lib_dir)/libFH.a
 lib_short=FH
-#tar_name=$(name).tar
-tar_name=$(name).tar.gz
+tar_name=$(name).tar
+#tar_name=$(name).tar.gz
 remote_url=http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/newversion/
 remote=$(remote_url)/$(tar_name)
 tarfile=$(TAR_DIR)/$(tar_name)
