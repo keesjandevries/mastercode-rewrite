0ad78a5ea863ce392466636d049b1cab9654f48b
Keesjan de Vries <keesjan777@gmail.com>
2013-06-04 10:39:57 +0100

    more investigation into minuite still has to be done

--- test_minuit.py
+++ test_minuit.py
@@ -13,13 +13,17 @@ def parse_args():
     parser.add_argument('--data-set' , default="mc8", help='data set for X^2 calculation')
     return parser.parse_args()
 
-def cmssm_chi2(parameters):
-    input_pars=inputs.get_mc_cmssm_inputs(*parameters)
+def f(xx,y):
+    return ((xx-2)/3)**2+y**2+y**4
+
+
+def cmssm_chi2(m0,m12,c,d,e,f,g):
+    input_pars=inputs.get_mc_cmssm_inputs(m0,m12,c,d,e,f,g)
     slha_obj, observables ,stdouts= point.run_point(**input_pars)
     #pass this constraints list to the chi2 function
     total, breakdown = Analyse.chi2(observables,constraints)
     return total
-    
+   
 if __name__ == '__main__':
     args=parse_args()
     all_constraints=Constraints_list.constraints
@@ -31,5 +35,9 @@ if __name__ == '__main__':
         data_set=[]
     constraints={name: all_constraints[name] for name in data_set}
 
-
-    print(cmssm_chi2([271.378279475, 920.368119935, 14.4499538001, -1193.57068242, 173.474173, 91.1877452551, 0.0274821578423]))
+    parameters=[271.378279475, 920.368119935, 14.4499538001, -1193.57068242, 173.474173, 91.1877452551, 0.0274821578423]
+    m = minuit2.Minuit2(cmssm_chi2,m0=271.378279475,m12=920.368119935,c=14.4499538001,d=-1193.57068242,e=173.474173,
+            f=91.1877452551,g=0.0274821578423)
+#    m = minuit2.Minuit2(f, xx=1,y=1)
+    m.printMode=1
+    m.migrad()
