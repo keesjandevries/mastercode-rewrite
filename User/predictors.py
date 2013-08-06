287e2eebb7300b3569738865cee04f3bfc484e3d
Keesjan de Vries <keesjan777@gmail.com>
2013-08-06 16:30:13 +0100

    good example of only public codes

--- User/predictors.py
+++ User/predictors.py
@@ -8,12 +8,21 @@ def get(name=None):
 softsusy='ObsCalculator.interfaces.softsusy'
 feynhiggs='ObsCalculator.interfaces.feynhiggs'
 micromegas='ObsCalculator.interfaces.micromegas'
+bphysics='ObsCalculator.interfaces.bphysics'
+susypope='ObsCalculator.interfaces.susypope'
+lspscat='ObsCalculator.interfaces.lspscat'
+superiso='ObsCalculator.interfaces.superiso'
 
 #collectoins of predictors
 predictors={
         'default':{
             'spectrum_generator':softsusy,
             'spectrum_modifiers':[feynhiggs],
-            'predictors':[feynhiggs,micromegas],
-            }
+            'predictors':[feynhiggs,micromegas,bphysics,susypope,lspscat],
+            },
+        'public':{
+            'spectrum_generator':softsusy,
+            'spectrum_modifiers':[feynhiggs],
+            'predictors':[feynhiggs,micromegas,superiso],
+            },
         }
