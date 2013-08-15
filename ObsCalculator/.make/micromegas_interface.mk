320b4f61196e845d754c5faf78589e38f6df4045
Keesjan de Vries <keesjan777@gmail.com>
2013-08-15 14:18:44 +0100

    micromegas interface make file libs indepenent of version

--- ObsCalculator/.make/micromegas_interface.mk
+++ ObsCalculator/.make/micromegas_interface.mk
@@ -8,17 +8,9 @@ interface_obj=$(interface_src:.cc=.o)
 
 $(interface_bin): $(interface_src)
 	$(cc) -fsigned-char -Wall -fPIC  -o $(interface_bin) \
-		-I$(inc)\
 		$(interface_src) \
-		$(lib) \
-		$(src_dir)/MSSM/lib/aLib.a \
-		$(src_dir)/MSSM/work/work_aux.a \
-		$(src_dir)/CalcHEP_src/lib/dynamic_me.a \
-		$(src_dir)/CalcHEP_src/lib/libSLHAplus.a \
-		$(src_dir)/CalcHEP_src/lib/num_c.a \
-		$(src_dir)/CalcHEP_src/lib/serv.a \
-		$(src_dir)/CalcHEP_src/lib/sqme_aux.so \
-		-lX11  -rdynamic -ldl -lm
+		-I$(inc)\
+		$(libs)	
 
 clean:
 	-rm -f $(interface_obj) $(interface_bin)
