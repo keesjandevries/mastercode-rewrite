efdc362f4c83c56e6a2232ffc50e71500c2ae401
Keesjan de Vries <keesjan777@gmail.com>
2013-08-15 14:15:12 +0100

    introduces versions of feynhiggs

--- ObsCalculator/.make_defs/feynhiggs.mk
+++ ObsCalculator/.make_defs/feynhiggs.mk
@@ -1,5 +1,7 @@
-version=2.8.7
+version=2.8.6
+#version=2.8.7
 #version=2.9.5
+#version=2.9.5.r3456v3
 name=FeynHiggs-$(version)
 src_dir=$(PREDICTOR_DIR)/$(name)
 machine=$(shell uname -m)
@@ -9,14 +11,14 @@ else
 	machine_lib_dir=lib
 endif
 lib_dir=$(INSTALL_DIR)/$(machine_lib_dir)
-lib=$(lib_dir)/libFH.a
-lib_short=FH
-tar_name=$(name).tar
-#tar_name=$(name).tar.gz
+lib_short=FH$(version)
+lib=$(lib_dir)/lib$(lib_short).a
+#tar_name=$(name).tar
+tar_name=$(name).tar.gz
 remote_url=http://wwwth.mpp.mpg.de/members/heinemey/feynhiggs/newversion/
 remote=$(remote_url)/$(tar_name)
 tarfile=$(TAR_DIR)/$(tar_name)
 interface_src=$(INTERFACE_DIR)/feynhiggs$(version).cc
-interface_lib=$(LIB_DIR)/libmcfeynhiggs.so
+interface_lib=$(LIB_DIR)/libmcfeynhiggs$(version).so
 
 cc=g++
