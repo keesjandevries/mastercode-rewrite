32688f8b4a57fa2f8054b03015bb527508b5b5b1
Keesjan de Vries <keesjan777@gmail.com>
2013-09-05 13:33:15 +0100

    add model with sign of mu negative

--- ObsCalculator/inputs.py
+++ ObsCalculator/inputs.py
@@ -22,6 +22,27 @@ def get_mc_cmssm_inputs(m0,m12,tanb,A0,mt,mz,Delta_alpha_had ):
                 },
             }
 
+def get_mc_neg_mu_cmssm_inputs(m0,m12,tanb,A0,mt,mz,Delta_alpha_had ):
+    return {
+            'SoftSUSY':{
+                'model'         :     'cMSSM'  ,
+                ('MINPAR', 'M0'):       m0,
+                ('MINPAR', 'M12'):      m12,
+                ('MINPAR', 'TB'):       tanb,
+                ('MINPAR', 'A'):        A0,
+                ('MINPAR', 'signMUE'):  -1,
+                ('SMINPUTS', 'Mt') :    mt,
+                },
+            'mc_slha_update':{
+                ('SMINPUTS','MZ')   : mz, 
+                },
+            'SUSY-POPE':{
+                'non_slha_inputs':{
+                    'DeltaAlfa5had' : Delta_alpha_had,
+                    }
+                },
+            }
+
 def get_mc_nuhm1_inputs(m0,m12,tanb,A0,mh2,mt,mz,Delta_alpha_had):
     return {
             'SoftSUSY':{
@@ -30,6 +51,31 @@ def get_mc_nuhm1_inputs(m0,m12,tanb,A0,mh2,mt,mz,Delta_alpha_had):
                 ('MINPAR', 'M12'):      m12,
                 ('MINPAR', 'TB'):       tanb,
                 ('MINPAR', 'A'):        A0,
+                ('SMINPUTS', 'Mt') :    mt,
+                ('EXTPAR','MH2') :       mh2,
+                },
+            'mc_slha_update':{
+                ('SMINPUTS','MZ')   : mz, 
+                },
+            'SUSY-POPE':{
+                'non_slha_inputs':{
+                    'DeltaAlfa5had' : Delta_alpha_had,
+                    }
+                },
+            'FeynHiggs':{
+                'drop_extpar' : True,
+                }
+            }
+
+def get_mc_neg_mu_nuhm1_inputs(m0,m12,tanb,A0,mh2,mt,mz,Delta_alpha_had):
+    return {
+            'SoftSUSY':{
+                'model'         :       'NUHM1',
+                ('MINPAR', 'M0'):       m0,
+                ('MINPAR', 'M12'):      m12,
+                ('MINPAR', 'TB'):       tanb,
+                ('MINPAR', 'A'):        A0,
+                ('MINPAR', 'signMUE'):  -1,
                 ('SMINPUTS', 'Mt') :    mt,
                 ('EXTPAR','MH2') :       mh2,
                 },
